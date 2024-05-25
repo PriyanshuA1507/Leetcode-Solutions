a0536f2f475821c6a4c00c538e809d80a6f6b80a
PriyanshuA1507 <97669354+PriyanshuA1507@users.noreply.github.com>
2024-05-25 13:29:37 +0530

    Time: 4 ms (32.03%) | Memory: 9 MB (66.81%) - LeetSync

---
+++ 140-word-break-ii/word-break-ii.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <unordered_set>
+
+class Solution {
+public:
+  
+    bool wordExists(string &word, const unordered_set<string> &wordDict) {
+        return wordDict.find(word) != wordDict.end();
+    }
+
+    void solverec(const string &s, unordered_set<string> &wordDict, string current, vector<string> &ans) {
+        if (s.empty()) {
+            ans.push_back(current.substr(1)); 
+            return;
+        }
+        
+        for (int i = 1; i <= s.size(); ++i) {
+            std::string prefix = s.substr(0, i);
+            if (wordExists(prefix, wordDict)) {
+                solverec(s.substr(i), wordDict, current + " " + prefix, ans);
+            }
+        }
+    }
+
+    vector<string> wordBreak(string s, vector<string>& wordDict) {
+        unordered_set<string> wordSet(wordDict.begin(), wordDict.end());
+        vector<string> ans;
+        solverec(s, wordSet, "", ans);
+        return ans;
+    }
+};
