6711468af064c838cada03d226cf62a2c77ffebe
PriyanshuA1507 <97669354+PriyanshuA1507@users.noreply.github.com>
2024-05-28 17:31:38 +0530

    Time: 84 ms (84.90%) | Memory: 27 MB (17.11%) - LeetSync

--- 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.cpp
+++ 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.cpp
@@ -3,7 +3,7 @@ public:
     int rangeSum(vector<int>& nums, int n, int left, int right) {
         long long ans=0;
         vector<int> sum;
-        for(int i=0;i<nums.size();i++){
+        /*for(int i=0;i<nums.size();i++){
          for(int j=i;j<nums.size();j++){
             int sum1 = 0;
         for(int k=i;k<=j;k++){
@@ -11,6 +11,13 @@ public:
         }
         sum.push_back(sum1);
          }
+        }*/
+        for (int i = 0; i < nums.size(); i++) {
+            int currentSum = 0;
+            for (int j = i; j < nums.size(); j++) {
+                currentSum += nums[j];
+                sum.push_back(currentSum);
+            }
         }
         sort(sum.begin(),sum.end());
         for(int i = left-1;i<right;i++){
