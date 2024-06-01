652e1c492347af49141a73b3d429c853c85f7fcc
PriyanshuA1507 <97669354+PriyanshuA1507@users.noreply.github.com>
2024-06-01 13:02:21 +0530

    Time: 199 ms (20.95%) | Memory: 92.9 MB (51.40%) - LeetSync

--- 2310-minimum-operations-to-halve-array-sum/minimum-operations-to-halve-array-sum.cpp
+++ 2310-minimum-operations-to-halve-array-sum/minimum-operations-to-halve-array-sum.cpp
@@ -5,7 +5,7 @@ public:
         priority_queue<double> pq;
         double totalSum = 0;
         
-        // Initialize the priority queue and calculate the total sum
+        
         for (int num : nums) {
             pq.push(num);
             totalSum += num;
@@ -14,7 +14,7 @@ public:
         double target = totalSum / 2;
         double currentSum = totalSum;
         
-        // Reduce the sum until it is less than or equal to half of the original sum
+       
         while (currentSum > target) {
             double maxElement = pq.top();
             pq.pop();
