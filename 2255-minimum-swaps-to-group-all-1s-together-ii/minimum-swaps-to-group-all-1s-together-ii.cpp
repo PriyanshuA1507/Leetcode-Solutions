4dac9ab8ce3d38a92d7baf27cf9a2e6eb097113b
PriyanshuA1507 <97669354+PriyanshuA1507@users.noreply.github.com>
2024-06-11 16:00:05 +0530

    Time: 74 ms (32.01%) | Memory: 89.6 MB (27.20%) - LeetSync

--- 2255-minimum-swaps-to-group-all-1s-together-ii/minimum-swaps-to-group-all-1s-together-ii.cpp
+++ 2255-minimum-swaps-to-group-all-1s-together-ii/minimum-swaps-to-group-all-1s-together-ii.cpp
@@ -1,25 +1,35 @@
 class Solution {
 public:
     int minSwaps(vector<int>& nums) {
-        int n = nums.size();
-        int ones = 0; // total no. of ones
-        for(int i=0;i<n;i++){
-            if(nums[i]==1) ones++;
+        vector<int>arr(nums.size()*2,0);
+        int ones=0;
+        for(int i=0;i<nums.size();i++){
+            arr[i]=nums[i];
+            arr[nums.size()+i]=nums[i];
+            if(nums[i]==1){
+                ones++;
+            }
         }
-        if(ones<=1) return 0;
-        
-        int k = ones; // sliding window length
-        int maxOnes = 0; // maxOnes in sub array of length k 
-        int cnt = 0;
-        for(int i=0;i<k;i++){
-            if(nums[i]==1) cnt++;
+        if(ones<=1){
+            return 0;
         }
-        maxOnes = cnt;
-        for(int i=k;i<n+k;i++){
-            if(nums[i-k]==1) cnt--; // if element removing from window is 1, then decrease count.
-            if(nums[i%n]==1) cnt++; // if element adding to window is 1, then increase count.
-            maxOnes = max(maxOnes,cnt); // maintaing maxOnes for all sub arrays of length k.
+        int zeros=0;
+        int i=0;
+        int j=0;
+        int ans=nums.size();
+        while(j<arr.size()){
+            if(!arr[j]){
+                zeros++;
+            }
+            if(j-i+1==ones){
+                ans=min(ans,zeros);
+                if(arr[i]==0){
+                    zeros--;
+                }
+                i++;
+            }
+            j++;
         }
-        return(k - maxOnes); // (total length of subarray - ones in the sub array found) 
+        return ans;
     }
 };
