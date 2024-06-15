44c42c0fb2a494ed3a6c762349d7faa8db174246
PriyanshuA1507 <97669354+PriyanshuA1507@users.noreply.github.com>
2024-06-15 18:21:57 +0530

    Time: 158 ms (32.27%) | Memory: 85.5 MB (63.02%) - LeetSync

--- 502-ipo/ipo.cpp
+++ 502-ipo/ipo.cpp
@@ -1,39 +1,28 @@
-
 class Solution {
 public:
     int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
-       
-        auto comp = [](const pair<int, int>& a, const pair<int, int>& b) {
+        int ans =0;
+            auto comp = [](const pair<int, int>& a, const pair<int, int>& b) {
             return a.first > b.first;
         };
-        priority_queue<pair<int, int>, vector<pair<int, int>>, decltype(comp)> pq(comp);
-        
-       
-        for (int i = 0; i < profits.size(); ++i) {
-            pq.push({capital[i], profits[i]});
+        priority_queue<pair<int,int>,vector<pair<int,int>>,decltype(comp)>pq(comp);
+        for(int i=0;i<profits.size();i++){
+            pq.push({capital[i],profits[i]});
         }
-        
-      
+        int i=0;
         priority_queue<int> pq1;
-        
-        
-        for (int i = 0; i < k; ++i) {
+       for(int i=0;i<k;++i){
+        while(!pq.empty() && pq.top().first<=w){
+            pq1.push(pq.top().second);
+            pq.pop();
           
-            while (!pq.empty() && pq.top().first <= w) {
-                pq1.push(pq.top().second);
-                pq.pop();
-            }
-            
-           
-            if (pq1.empty()) {
-                break;
-            }
-            
-           
-            w += pq1.top();
-            pq1.pop();
         }
-        
+        if(pq1.empty()){
+            break;
+        }
+        w+=pq1.top();
+        pq1.pop();
+        }
         return w;
     }
 };
