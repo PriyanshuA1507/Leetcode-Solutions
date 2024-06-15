94d1203c67204ceaea05e5cf881199339e011ba4
PriyanshuA1507 <97669354+PriyanshuA1507@users.noreply.github.com>
2024-06-15 23:00:33 +0530

    Time: 157 ms (70.75%) | Memory: 95.6 MB (49.97%) - LeetSync

--- 2636-maximum-subsequence-score/maximum-subsequence-score.cpp
+++ 2636-maximum-subsequence-score/maximum-subsequence-score.cpp
@@ -7,28 +7,25 @@ public:
         for (int i = 0; i < nums1.size(); i++) {
             a.push_back({nums2[i], nums1[i]});
         }
-        sort(a.begin(), a.end(), greater<>());
-
-       
-        priority_queue<int, vector<int>, greater<int>> minHeap;
-        long long sum = 0, ans = 0;
-
-        
-        for (int i = 0; i < a.size(); i++) {
-            minHeap.push(a[i].second);
-            sum += a[i].second;
-
-           
-            if (minHeap.size() > k) {
-                sum -= minHeap.top();
-                minHeap.pop();
-            }
-
-            if (minHeap.size() == k) {
-                ans = max(ans, sum * a[i].first);
-            }
+        long long ans =0;
+        long long sum=0;
+        sort(a.begin(), a.end());
+        reverse(a.begin(),a.end());
+       priority_queue<int,vector<int>,greater<int>> pq;
+       for(int i=0;i<nums2.size();i++){
+        sum+=a[i].second;
+        pq.push(a[i].second);
+         
+        if(pq.size()>k){
+        sum-=pq.top();
+        pq.pop();
         }
-
-        return ans;
+        
+        if(pq.size()==k){
+            ans = max(ans, sum*a[i].first);
+        }
+        
+       }
+       return ans;
     }
 };
