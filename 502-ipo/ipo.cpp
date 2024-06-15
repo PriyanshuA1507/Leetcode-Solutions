1244c9222d0465630df457d6e2ebcc28e52bc625
PriyanshuA1507 <97669354+PriyanshuA1507@users.noreply.github.com>
2024-06-15 18:17:55 +0530

    Time: 165 ms (24.96%) | Memory: 85.6 MB (42.83%) - LeetSync

--- 502-ipo/ipo.cpp
+++ 502-ipo/ipo.cpp
@@ -1,25 +1,43 @@
+#include <vector>
+#include <queue>
+
+using namespace std;
+
 class Solution {
 public:
     int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
-        int n = profits.size();
-        vector<pair<int, int>> projects;
-        for (int i = 0; i < n; i++) {
-            projects.push_back({capital[i], profits[i]});
+        // Priority queue to store projects based on the capital required
+        auto comp = [](const pair<int, int>& a, const pair<int, int>& b) {
+            return a.first > b.first;
+        };
+        priority_queue<pair<int, int>, vector<pair<int, int>>, decltype(comp)> pq(comp);
+        
+        // Fill the priority queue with initial projects
+        for (int i = 0; i < profits.size(); ++i) {
+            pq.push({capital[i], profits[i]});
         }
-        sort(projects.begin(), projects.end());
-         priority_queue<int> pq;
-         int i=0;
-        while(k-->0){
-            while(i<n && projects[i].first<=w){
-                pq.push(projects[i].second);
-                i++;
+        
+        // Priority queue to store available projects based on profits
+        priority_queue<int> pq1;
+        
+        // Iterate k times to maximize the capital
+        for (int i = 0; i < k; ++i) {
+            // Move projects that can be done with current capital to pq1
+            while (!pq.empty() && pq.top().first <= w) {
+                pq1.push(pq.top().second);
+                pq.pop();
             }
-            if(pq.empty()){
+            
+            // If no projects can be done, break the loop
+            if (pq1.empty()) {
                 break;
             }
-            w+=pq.top();
-            pq.pop();
+            
+            // Take the most profitable project
+            w += pq1.top();
+            pq1.pop();
         }
+        
         return w;
     }
 };
