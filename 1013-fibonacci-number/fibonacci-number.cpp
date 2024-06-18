7b132310878277aa723639b933933654774e4588
PriyanshuA1507 <97669354+PriyanshuA1507@users.noreply.github.com>
2024-06-18 10:24:39 +0530

    Time: 3 ms (66.56%) | Memory: 7.2 MB (33.84%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,12 +1,18 @@
 class Solution {
 public:
+    int helper(int n, vector<int>& a) {
+        if (n <= 1) {
+            return n;
+        }
+        if (a[n] != -1) {
+            return a[n];
+        }
+        a[n] = helper(n - 1, a) + helper(n - 2, a);
+        return a[n];
+    }
+
     int fib(int n) {
-        if(n==0){
-            return 0;
-        }
-        if(n==1){
-            return 1;
-        }
-        return fib(n-1)+fib(n-2);
+        vector<int> a(n + 1, -1);
+        return helper(n, a);
     }
 };
