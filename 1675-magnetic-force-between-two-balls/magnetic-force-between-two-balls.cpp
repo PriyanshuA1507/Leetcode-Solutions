f0924e11912214fc0410af2826de990ff1d0de88
PriyanshuA1507 <97669354+PriyanshuA1507@users.noreply.github.com>
2024-06-20 12:26:17 +0530

    Time: 129 ms (43.95%) | Memory: 61.4 MB (35.79%) - LeetSync

--- 1675-magnetic-force-between-two-balls/magnetic-force-between-two-balls.cpp
+++ 1675-magnetic-force-between-two-balls/magnetic-force-between-two-balls.cpp
@@ -1,39 +1,36 @@
 class Solution {
 public:
-    bool ip(vector<int>& position, int m, int mid) {
-        int mc = 1; 
-        int lastpos = position[0];
-        
-        for (int i = 1; i < position.size(); i++) {
-            if (position[i] - lastpos >= mid) {
-                mc++;
-                if (mc == m) {
-                    return true;
-                }
-                lastpos = position[i];
-            }
+    bool ip(vector<int>& position, int m,int mid){
+     int mc=1;
+     int lastpos = position[0];
+     for(int i=1;i<position.size();i++){
+     if(position[i]-lastpos>=mid){
+        mc++;
+        if(mc==m){
+            return true;
         }
-        return false;
+        lastpos = position[i];
+     }
+     }
+     return false;
     }
-    
     int maxDistance(vector<int>& position, int m) {
-        sort(position.begin(), position.end());
+        sort(position.begin() , position.end());
+        int s= 0;
         int n = position.size();
-        int s = 0;
-        int e = position[n-1] - position[0]; 
-        int ans = 0;
-        int mid;
-        
-        while (s <= e) {
-            mid = s + (e - s) / 2;
-            if (ip(position, m, mid)) {
-                ans = mid;
-                s = mid + 1;
-            } else {
-                e = mid - 1;
-            }
+        int e = position[n-1]-position[0];
+        int ans =0;
+        int mid = s+ (e-s)/2;
+        while(s<=e){
+          if(ip(position,m,mid)){
+            ans = mid;
+            s = mid+1;
+          }
+          else{
+            e = mid-1;
+          }
+          mid = s + (e-s)/2;
         }
-        
         return ans;
     }
 };
