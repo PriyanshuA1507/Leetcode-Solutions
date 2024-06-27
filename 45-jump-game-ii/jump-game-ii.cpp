1d542984b22cc1e17e0a68440479e6cad2477e06
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2024-06-27 14:30:06 +0530

    Time: 196 ms (12.37%) | Memory: 20.5 MB (9.76%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -0,0 +1,31 @@
+#include <vector>
+#include <climits>
+#include <algorithm>
+
+class Solution {
+public:
+    int solve(std::vector<int>& nums, int index, int n, vector<int>& memo) {
+        if (index >= n - 1) {
+            return 0;
+        }
+        if (memo[index] != -1) {
+            return memo[index];
+        }
+        int ans = INT_MAX;
+        for (int i = 1; i <= nums[index]; ++i) {
+            if (index + i < n) {
+                int temp = solve(nums, index + i, n, memo);
+                if (temp != INT_MAX) {
+                    ans = min(ans, 1 + temp);
+                }
+            }
+        }
+        return memo[index] = ans;
+    }
+
+    int jump(std::vector<int>& nums) {
+        int n = nums.size();
+        vector<int> memo(n, -1);
+        return solve(nums, 0, n, memo);
+    }
+};
