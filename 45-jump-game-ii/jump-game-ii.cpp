67d27dfa1c7e52edfb1b3824bd38f55174254b83
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2024-06-27 14:31:32 +0530

    Time: 207 ms (11.02%) | Memory: 20.6 MB (7.52%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,31 +1,27 @@
-#include <vector>
-#include <climits>
-#include <algorithm>
-
 class Solution {
 public:
-    int solve(vector<int>& nums, int index, int n, vector<int>& memo) {
-        if (index >= n - 1) {
+    int solve(vector<int>& nums, int index, int n,vector<int>& dp) {
+        if (index >= n-1) {
             return 0;
         }
-        if (memo[index] != -1) {
-            return memo[index];
+        if(dp[index]!=-1){
+            return dp[index];
         }
         int ans = INT_MAX;
         for (int i = 1; i <= nums[index]; ++i) {
             if (index + i < n) {
-                int temp = solve(nums, index + i, n, memo);
-                if (temp != INT_MAX) {
+                int temp = solve(nums, index + i, n,dp);
+                if (temp  != INT_MAX) {
                     ans = min(ans, 1 + temp);
                 }
             }
         }
-        return memo[index] = ans;
+        return dp[index]= ans;
     }
 
-    int jump(std::vector<int>& nums) {
+    int jump(vector<int>& nums) {
         int n = nums.size();
-        vector<int> memo(n, -1);
-        return solve(nums, 0, n, memo);
+        vector<int> dp(n,-1);
+        return solve(nums, 0, n,dp);
     }
 };
