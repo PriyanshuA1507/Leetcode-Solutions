505636ef8ee8f3d1a31725f59231534739480e48
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2024-06-28 18:41:05 +0530

    Time: 74 ms (21.65%) | Memory: 60.3 MB (16.06%) - LeetSync

--- 1743-count-substrings-that-differ-by-one-character/count-substrings-that-differ-by-one-character.cpp
+++ 1743-count-substrings-that-differ-by-one-character/count-substrings-that-differ-by-one-character.cpp
@@ -1,25 +1,29 @@
 class Solution {
 public:
- int solve(int i,int j,bool diff,string s,string t){
+ int solve(int i,int j,bool diff,string s,string t,vector<vector<vector<int>>>&dp){
     if(i==s.size()|| j==t.size()){
         return 0;
     }
+    if(dp[i][j][diff]!=-1){
+        return dp[i][j][diff];
+    }
     if(diff){
       if(s[i]!=t[j]){
-        return 0;
+        return dp[i][j][diff]=0;
       } 
-      return 1 + solve(i+1,j+1,diff,s,t);
+      return dp[i][j][diff]=1 + solve(i+1,j+1,diff,s,t,dp);
     }
     if(s[i]!=t[j]){
-        return 1 + solve(i+1,j+1,true,s,t);
+        return dp[i][j][diff]= 1 + solve(i+1,j+1,true,s,t,dp);
     }
-    return solve(i+1,j+1,diff,s,t);
+    return dp[i][j][diff]=solve(i+1,j+1,diff,s,t,dp);
  }
     int countSubstrings(string s, string t) {
-        int ans =0;
+    int ans =0;
+    vector<vector<vector<int>>> dp(s.size(),vector<vector<int>>(t.size(),vector<int>(2,-1)));
      for(int i=0;i<s.size();i++){
     for(int j=0;j<t.size();j++){
-       ans +=solve(i,j,false,s,t);
+       ans +=solve(i,j,false,s,t,dp);
     }
      }
      return ans;   
