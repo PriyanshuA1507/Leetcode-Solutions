3753deb42e4ce3df41f0a16205b45dde9f82fbaa
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2024-07-06 13:41:05 +0530

    Time: 119 ms (40.18%) | Memory: 103.9 MB (30.01%) - LeetSync

---
+++ 1129-longest-string-chain/longest-string-chain.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int solve(vector<string>& words, int ci, int pi, vector<vector<int>>& dp) {
+        if (ci >= words.size()) {
+            return 0;
+        }
+        if (dp[ci][pi + 1] != -1) {
+            return dp[ci][pi + 1];
+        }
+        int take = 0;
+        if (pi == -1) {
+            take = 1 + solve(words, ci + 1, ci, dp);
+        } else if (words[ci].size() == words[pi].size() + 1) {
+            int i = 0;
+            int j = 0;
+            while (i < words[ci].size() && j < words[pi].size()) {
+                if (words[ci][i] == words[pi][j]) {
+                    j++;
+                }
+                i++;
+            }
+            if (j == words[pi].size()) {
+                take = 1 + solve(words, ci + 1, ci, dp);
+            }
+        }
+        int nottake = solve(words, ci + 1, pi, dp);
+        return dp[ci][pi + 1] = max(take, nottake);
+    }
+
+    int longestStrChain(vector<string>& words) {
+        sort(words.begin(), words.end(), [](const string& a, const string& b) {
+            return a.size() < b.size();
+        });
+        vector<vector<int>> dp(words.size(), vector<int>(words.size() + 1, -1));
+        return solve(words, 0, -1, dp);
+    }
+};
