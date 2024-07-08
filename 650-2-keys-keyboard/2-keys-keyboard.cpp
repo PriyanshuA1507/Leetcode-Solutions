3e14b962822ea7d3edb7f2181556c3b6719fb410
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2024-07-08 18:58:32 +0530

    Time: 17 ms (36.30%) | Memory: 7.1 MB (70.01%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.cpp
+++ 650-2-keys-keyboard/2-keys-keyboard.cpp
@@ -6,9 +6,9 @@ public:
         if(n==i){
             return 0;
         }
-        int c = 1+ solve(n,2*i,i);
-        int  p =  solve(n,i+j,j);
-        return 1+min(c,p);
+        int c = 2+ solve(n,2*i,i);
+        int  p = 1+solve(n,i+j,j);
+        return min(c,p);
     }
     int minSteps(int n) {
         if(n<=1){
