c971dc71a85bd17e2b0f6990fb070ccbc2240a9f
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2024-07-08 22:21:19 +0530

    Time: 255 ms (12.47%) | Memory: 136.7 MB (13.86%) - LeetSync

--- 1748-best-team-with-no-conflicts/best-team-with-no-conflicts.cpp
+++ 1748-best-team-with-no-conflicts/best-team-with-no-conflicts.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
-    int solve(vector<pair<int, int>>& a, int index, int last_index, vector<vector<int>>& dp) {
-        if (index >= a.size()) {
+    int solve(vector<pair<int,int>>& a,int index,int pi,vector<vector<int>>& dp){
+        if(index>=a.size()){
             return 0;
         }
-        if (dp[index][last_index + 1] != -1) {
-            return dp[index][last_index + 1];
+        if(dp[index][pi+1]!=-1){
+            return dp[index][pi+1];
         }
         int take = 0;
-        if (last_index == -1 || a[last_index].second <= a[index].second) {
-            take = a[index].second + solve(a, index + 1, index, dp);
+        if(pi==-1 || a[pi].second<=a[index].second){
+         take = a[index].second + solve(a,index+1,index,dp);
         }
-        int nottake = solve(a, index + 1, last_index, dp);
-        return dp[index][last_index + 1] = max(take, nottake);
+        int nottake = solve(a,index+1,pi,dp);
+        return dp[index][pi+1]=max(take,nottake);
     }
-
     int bestTeamScore(vector<int>& scores, vector<int>& ages) {
-        vector<pair<int, int>> a;
-        for (int i = 0; i < ages.size(); i++) {
-            a.push_back({ages[i], scores[i]});
+        vector<pair<int,int>> a;
+        for(int i=0;i<ages.size();i++){
+            a.push_back({ages[i],scores[i]});
         }
-        sort(a.begin(), a.end());
-        vector<vector<int>> dp(a.size(), vector<int>(a.size() + 1, -1));
-        return solve(a, 0, -1, dp);
+        sort(a.begin(),a.end());
+        vector<vector<int>> dp(a.size()+1,vector<int>(a.size()+1,-1));
+        return solve(a,0,-1,dp);
     }
 };
