f343b99003f94ebe3ebc15979a98ff46d0abafd7
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2024-08-05 18:14:20 +0530

    Time: 302 ms (5.12%) | Memory: 83 MB (5.05%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+  typedef pair<int,pair<int,int>> P;
+   /* int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int ans = 0;
         int n = grid.size();
           if (n == 0 || grid[0][0] == 1 || grid[n-1][n-1] == 1) return -1;
@@ -27,5 +28,36 @@ public:
         }
         }
          return -1;
-    }
+    }*/
+      int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+        vector<vector<int>> direction = {{-1,0},{0,1},{1,0},{0,-1}, {-1,1}, {1,1}, {1,-1}, {-1,-1}};
+       int n = grid.size() ,m = grid[0].size();
+       if(m==0 || n==0 || grid[0][0]!=0){
+        return -1;
+       }
+       vector<vector<int>> result(m,vector<int>(n,INT_MAX));
+       priority_queue<P, vector<P>, greater<P>> pq;
+       pq.push({0,{0,0}});
+       result[0][0]=0;
+       while(!pq.empty()){
+        int d = pq.top().first;
+        pair<int,int> n1 = pq.top().second;
+        int x = n1.first;
+        int y = n1.second;
+        pq.pop();
+        for(auto dr: direction){
+        int r = x + dr[0];
+        int c = y + dr[1];
+        int dist = 1;
+        if(r>=0 && c>=0 && r<n && c<m && grid[r][c]==0 && d+dist<result[r][c]){
+        pq.push({d+dist,{r,c}});
+        result[r][c] = d + dist;
+        }
+        }
+       }
+       if(result[m-1][n-1]==INT_MAX)
+       return -1;
+
+       return result[m-1][n-1]+1;
+      }
 };
