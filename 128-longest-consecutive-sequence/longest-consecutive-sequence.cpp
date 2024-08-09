802b953091a930b72411511ca030cb3239fca2a4
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2024-08-09 18:20:51 +0530

    Time: 155 ms (40.43%) | Memory: 75.7 MB (13.28%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -10,22 +10,19 @@ public:
 
         std::set<int> a(nums.begin(), nums.end());
         int length = 0;
-        int start = *a.begin();  // Initialize start with the first element in the set
-        int ans = 1;  // Start with 1 because a single number is a valid sequence
+        int start = *a.begin();  
+        int ans = 1;  
 
         for(int it : a) {
             if(it == start) {
-                // Continue if it's the first element
                 length = 1;
             } else if(it == start + 1) {
-                // Continue the sequence
                 length++;
             } else {
-                // Reset the sequence if it breaks
                 length = 1;
             }
             start = it;
-            ans = std::max(ans, length);
+            ans =max(ans, length);
         }
 
         return ans;
