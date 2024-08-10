4d5cc9a3e83f848823e04da35529c812d8037ff3
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2024-08-10 17:55:53 +0530

    Time: 118 ms (51.78%) | Memory: 69.3 MB (13.05%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,44 +1,40 @@
 class Solution {
 public:
-    int minDays(vector<int>& bloomDay, int m, int k) {
-        if ((long long)m * k > bloomDay.size()) {
-            return -1;
+     bool possible(vector<int>& b, int m, int k,int h){
+        int ans = 0;
+        for(int i=0;i<b.size();i++){
+            int a=0;
+         while(i<b.size() && b[i]<=h && a<k){
+           a++;
+           i++;
+         }
+         if(a==k){
+            ans++;
+            i--;
+         }
         }
-
-        int low = 1, high = 1e9;
-        while (low < high) {
-            int mid = low + (high - low) / 2;
-
-            if (canMakeBouquets(bloomDay, m, k, mid)) {
-                high = mid;
-            } else {
-                low = mid + 1;
-            }
+        if(ans>=m){
+            return true;
         }
-
-        return low;
-    }
-
-private:
-    bool canMakeBouquets(vector<int>& bloomDay, int m, int k, int day) {
-        int total = 0;
-        for (int i = 0; i < bloomDay.size(); i++) {
-            int count = 0;
-            while (i < bloomDay.size() && count < k && bloomDay[i] <= day) {
-                count++;
-                i++;
-            }
-
-            if (count == k) {
-                total++;
-                i--;
-            }
-
-            if (total >= m) {
-                return true;
-            }
-        }
-
         return false;
+     }
+    int minDays(vector<int>& bloomday, int m, int k) {
+       if((long long)m*k>bloomday.size()){
+        return -1;
+       } 
+       int start = 0;
+       int end = INT_MAX;
+       int ans = -1;
+       while(start<=end){
+      int mid = (start+end)/2;
+       if(possible(bloomday,m,k,mid)){
+        ans = mid;
+        end = mid-1;
+       }
+       else{
+        start = mid+1;
+       }
+       }
+       return ans;
     }
 };
