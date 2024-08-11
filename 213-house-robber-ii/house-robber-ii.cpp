40fa4ea9667a85da192690d2189161c34c6a451a
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2024-08-11 12:27:07 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (30.04%) - LeetSync

---
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int solve(vector<int>& nums,int index,vector<int>& dp){
+        int n = nums.size();
+        if(index>=n){
+            return 0;
+        }
+        if(index==n-1){
+            return nums[n-1];
+        }
+        if(dp[index]!=-1){
+            return dp[index];
+        }
+
+        int take = nums[index] + solve(nums,index+2,dp);
+        int nottake  = solve(nums,index+1,dp);
+        return dp[index]=max(take,nottake);
+     }
+    int rob(vector<int>& nums) {
+        int n  = nums.size();
+        if(n==1){
+            return nums[0];
+        }
+        vector<int> a,b;
+        vector<int> dp1(n+1,-1);
+        vector<int> dp2(n+1,-1);
+        for(int i=0;i<nums.size()-1;i++){
+            a.push_back(nums[i]);
+        }
+        for(int i=1;i<nums.size();i++){
+            b.push_back(nums[i]);
+        }
+        return max(solve(a,0,dp1), solve(b,0,dp2));
+    }
+};
