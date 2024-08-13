641dd3430692b609e96c1a3c7da1c265b1f36023
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2024-08-14 01:44:20 +0530

    Time: 19 ms (23.28%) | Memory: 17 MB (15.34%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,31 +1,31 @@
 class Solution {
 public:
-   int solve(vector<int>& prices,int index,bool buy,int k,vector<vector<vector<int>>>& dp){
-    if(index==prices.size()){
+   int solve(vector<int>& prices,int index,int b,int k,vector<vector<vector<int>>>& dp){
+    if(index>=prices.size()){
         return 0;
     }
     if(k==0){
         return 0;
     }
-    if(dp[index][buy][k]!=-1){
-        return dp[index][buy][k];
+    if(dp[index][b][k]!=-1){
+        return dp[index][b][k];
     }
-    int ans =0;
-    if(buy){
-     int buykaro = -prices[index]  + solve(prices,index+1,0,k,dp);
-     int skipkaro = 0  + solve(prices,index+1,1,k,dp);
-     ans = max(buykaro,skipkaro);
-    }
-    if(!buy ){
-     int sellkaro = prices[index]  + solve(prices,index+1,1,k-1,dp);
-     int skipkaro = 0  + solve(prices,index+1,0,k,dp);
-       ans = max(sellkaro,skipkaro);
-    }
-    return dp[index][buy][k] = ans;
+    int profit = 0;
+        if(b==1){
+        int buykaro = -prices[index] + solve(prices,index+1,0,k,dp);
+        int skipkaro = 0 +solve(prices,index+1,1,k,dp);
+        profit = max(buykaro,skipkaro);
+        }
+        else{
+        int sellkaro = prices[index] + solve(prices,index+1,1,k-1,dp);
+        int skipkaro = 0 +solve(prices,index+1,0,k,dp);
+        profit = max(sellkaro , skipkaro);
+   }
+   return dp[index][b][k]=profit;
    }
     int maxProfit(int k, vector<int>& prices) {
-        int n = prices.size();
-        vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(3,vector<int>(k+1,-1)));
-       return solve(prices,0,1,k,dp); 
+         int n = prices.size();
+        vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2, vector<int>(k+1, -1)));
+        return solve(prices,0,1,k,dp);
     }
 };
