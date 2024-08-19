a9b8825355f32f2b2dee5e06670075e0209345d3
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2024-08-19 20:09:49 +0530

    Time: 104 ms (16.35%) | Memory: 9 MB (68.39%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -1,25 +1,19 @@
 class Solution {
 public:
-    int solve(vector<int>& nums, int si, int ei, int turn) {
+    int solve(vector<int>& nums, int si, int ei) {
         if (si > ei) {
             return 0;
         }
         
-        if (turn == 1) {
-
-            int scoreStart = nums[si] + solve(nums, si + 1, ei, 0);
-            int scoreEnd = nums[ei] + solve(nums, si, ei - 1, 0);
+        
+            int scoreStart = nums[si] + min(solve(nums, si + 2, ei),solve(nums,si+1,ei-1));
+            int scoreEnd = nums[ei] + min(solve(nums, si+1, ei - 1),solve(nums,si,ei-2));
             return max(scoreStart, scoreEnd);
-        } else {
-            int scoreStart = solve(nums, si + 1, ei, 1);
-            int scoreEnd = solve(nums, si, ei - 1, 1);
-            return min(scoreStart, scoreEnd);
-        }
     }
 
     bool predictTheWinner(vector<int>& nums) {
         int totalSum = accumulate(nums.begin(), nums.end(), 0);
-        int player1Score = solve(nums, 0, nums.size() - 1, 1);
+        int player1Score = solve(nums, 0, nums.size() - 1);
         return player1Score >= (totalSum - player1Score);
     }
 };
