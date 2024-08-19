9a61f2a979448d180356e0be2410541ef7c0fe6e
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2024-08-19 18:16:02 +0530

    Time: 27 ms (18.08%) | Memory: 157.3 MB (8.56%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.cpp
+++ 650-2-keys-keyboard/2-keys-keyboard.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
-    int solve(int n, int ol,int cl){
+    int solve(int n, int ol,int cl,vector<vector<int>>& dp){
         if(ol>n){
             return 1e9;
         }
         if(ol==n){
             return 0;
         }
-        
+        if(dp[ol][cl]!=-1){
+            return dp[ol][cl];
+        }
+       int c1 = 2 + solve(n,2*ol,ol,dp);
        
-       int c1 = 2 + solve(n,2*ol,ol);
-       
-       int p = 1 + solve(n,ol+cl,cl);
+       int p = 1 + solve(n,ol+cl,cl,dp);
 
-       return  min(c1,p);
+       return  dp[ol][cl]=min(c1,p);
     }
     int minSteps(int n) {
         if(n<=1){
@@ -21,6 +22,7 @@ public:
         }
        int ol = 1;
        int cl = 1;
-    return 1+solve(n,1,1);
+       vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
+    return 1+solve(n,1,1,dp);
     }
 };
