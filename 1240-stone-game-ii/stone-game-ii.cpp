67e128829c99e996f781eac4c21edae210503ae3
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2024-08-20 00:41:13 +0530

    Time: 590 ms (5.00%) | Memory: 656.5 MB (5.00%) - LeetSync

--- 1240-stone-game-ii/stone-game-ii.cpp
+++ 1240-stone-game-ii/stone-game-ii.cpp
@@ -1,33 +1,32 @@
 class Solution {
 public:
-    int solve(vector<int>& piles, int p, int index, int M, vector<vector<vector<int>>>& memo) {
+    int solve(vector<int> piles,int p,int index,int M,vector<vector<vector<int>>>& dp){
         int n = piles.size();
-        if (index >= n) return 0;
-        if (memo[p][index][M] != -1) return memo[p][index][M];
-
-        int stones = 0;
-        int result;
-
-        if (p == 1) {
-            result = INT_MIN;
-            for (int x = 1; x <= 2 * M && index + x <= n; ++x) {
-                stones += piles[index + x - 1];
-                result = max(result, stones + solve(piles, 0, index + x, max(M, x), memo));
-            }
-        } else {
-            result = INT_MAX;
-            for (int x = 1; x <= 2 * M && index + x <= n; ++x) {
-                result = min(result, solve(piles, 1, index + x, max(M, x), memo));
-            }
+        if(index>=n){
+            return 0;
         }
-
-        memo[p][index][M] = result;
-        return result;
+        if(dp[p][index][M]!=-1){
+            return dp[p][index][M];
+        }
+         int stones=0;
+        int result = (p==1)?-1:INT_MAX;
+        for(int x = 1;x<=2*M && index+x<=n;x++){
+            
+          stones+=piles[index+x-1];
+          if(p==1){
+            result = max(result, stones + solve(piles,0,index+x,max(M,x),dp));
+          }
+          else{
+             
+            result = min(result, solve(piles,1,index+x,max(M,x),dp));
+          }
+        }
+        return dp[p][index][M]=result;
     }
-
     int stoneGameII(vector<int>& piles) {
         int n = piles.size();
-        vector<vector<vector<int>>> memo(2, vector<vector<int>>(n, vector<int>(n + 1, -1)));
-        return solve(piles, 1, 0, 1, memo);
+       vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(n+1,vector<int>(n+1,-1)));
+      return solve(piles,1, 0, 1,dp);
+        
     }
 };
