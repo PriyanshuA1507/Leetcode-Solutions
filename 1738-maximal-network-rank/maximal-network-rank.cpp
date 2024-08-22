802ef1ece37f80843ddf0f836a5ef35c4f31311b
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2024-08-22 11:37:53 +0530

    Time: 66 ms (59.56%) | Memory: 35.2 MB (68.10%) - LeetSync

--- 1738-maximal-network-rank/maximal-network-rank.cpp
+++ 1738-maximal-network-rank/maximal-network-rank.cpp
@@ -1,28 +1,25 @@
 class Solution {
 public:
     int maximalNetworkRank(int n, vector<vector<int>>& roads) {
-        vector<int> rank(n, 0);
-        vector<vector<int>> adj(n, vector<int>(n, 0)); // Adjacency matrix
-
-        for (auto& a : roads) {
-            adj[a[0]][a[1]] = 1;
-            adj[a[1]][a[0]] = 1;
+        unordered_map<int,vector<int>> adj;
+        vector<int> rank(n,0);
+        for(auto& a:roads){
+            adj[a[0]].push_back(a[1]);
+            adj[a[1]].push_back(a[0]);
             rank[a[0]]++;
             rank[a[1]]++;
         }
-
         int maxi = INT_MIN;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i != j) { 
-                    int currentRank = rank[i] + rank[j];
-                    if (adj[i][j] == 1) { 
-                        currentRank--; 
-                    }
-                    maxi = max(maxi, currentRank);
+        for(int i=0;i<n;i++){
+          for(int j=0;j<n && j!=i;j++){
+         int rank1 = rank[i] + rank[j];
+         if (find(adj[i].begin(), adj[i].end(), j) != adj[i].end()) {
+                    rank1--; 
                 }
-            }
+        maxi = max(maxi, rank1);
+          }
         }
         return maxi;
+        
     }
 };
