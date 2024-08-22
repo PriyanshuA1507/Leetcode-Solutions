5934bf45b7f8da15e8dd199676bb5480b918352a
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2024-08-23 00:46:16 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (24.22%) - LeetSync

---
+++ 410-split-array-largest-sum/split-array-largest-sum.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    bool canwe(vector<int>& nums, int k, int mid) {
+        int n = nums.size();
+        int ds = 1;
+        int sum = 0;
+        
+        for (int i = 0; i < n; i++) {
+            if (sum + nums[i] > mid) {
+                ds++;
+                sum = nums[i];
+                if (ds > k) {
+                    return false;
+                }
+            } else {
+                sum += nums[i];
+            }
+        }
+        return true;
+    }
+
+    int splitArray(vector<int>& nums, int k) {
+        int sum = 0;
+        int maxi = INT_MIN;
+
+        for (int i = 0; i < nums.size(); i++) {
+            maxi = max(maxi, nums[i]);
+            sum += nums[i];
+        }
+
+        int low = maxi;
+        int high = sum;
+        int ans = high;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (canwe(nums, k, mid)) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+};
