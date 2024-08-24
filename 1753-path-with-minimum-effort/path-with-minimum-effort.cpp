10a05c18b852389e47a7b9755bd2f4ded8ac4f05
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2024-08-25 01:11:20 +0530

    Time: 82 ms (80.90%) | Memory: 24.1 MB (31.78%) - LeetSync

---
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+   typedef pair<int,pair<int,int>> p;
+    int minimumEffortPath(vector<vector<int>>& h) {
+        int n = h.size();
+        int m = h[0].size();
+    vector<vector<int>> direction = {{-1,0},{0,1},{1,0},{0,-1}};
+     vector<vector<int>> result(n,vector<int>(m,INT_MAX));
+     priority_queue<p,vector<p>,greater<p>>pq;
+     pq.push({0,{0,0}});
+     result[0][0] = 0;
+     while(pq.size()!=0){
+    auto a = pq.top();
+    pq.pop();
+    int diff = a.first;
+    int r = a.second.first;
+    int c = a.second.second;
+     if (r == n-1 && c == m-1) {
+                return diff;  
+            }
+    for(auto & d: direction){
+     int nr = r + d[0];
+     int nc = c + d[1];
+     if(nr>=0 && nc>=0 && nc<m && nr<n ){
+       int newEffort = max(diff, abs(h[r][c] - h[nr][nc]));
+         if (newEffort < result[nr][nc]) {
+             result[nr][nc] = newEffort;
+             pq.push({newEffort, {nr, nc}});
+        }
+     }
+    }
+     }
+     return result[n-1][n-1];
+    }
+};
