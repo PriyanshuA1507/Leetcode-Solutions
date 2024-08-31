40f8ba489f0101b8d8e92ee7044ab57b8e2f9639
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2024-08-31 18:08:03 +0530

    Time: 285 ms (97.90%) | Memory: 143.7 MB (93.03%) - LeetSync

--- 2403-count-unreachable-pairs-of-nodes-in-an-undirected-graph/count-unreachable-pairs-of-nodes-in-an-undirected-graph.cpp
+++ 2403-count-unreachable-pairs-of-nodes-in-an-undirected-graph/count-unreachable-pairs-of-nodes-in-an-undirected-graph.cpp
@@ -1,56 +1,39 @@
 class Solution {
 public:
-    int findparent(int x, vector<int>& parent) {
-        if (x == parent[x]) {
+    int findParent(vector<int>& parent, int x) {
+        if (parent[x] == x) {
             return x;
         }
-        return parent[x] = findparent(parent[x], parent);
+        return parent[x] = findParent(parent, parent[x]);
     }
 
-    void Union(int x, int y, vector<int>& parent, vector<int>& rank) {
-        int x_parent = findparent(x, parent);
-        int y_parent = findparent(y, parent);
-        if (x_parent == y_parent) {
-            return;
-        }
-        if (rank[x_parent] > rank[y_parent]) {
-            parent[y_parent] = x_parent;
-        } else if (rank[y_parent] > rank[x_parent]) {
-            parent[x_parent] = y_parent;
-        } else {
-            parent[y_parent] = x_parent;
-            rank[x_parent]++;
+    void unionSets(int x, int z, vector<int>& parent) {
+        int xParent = findParent(parent, x);
+        int zParent = findParent(parent, z);
+        if (xParent != zParent) {
+            parent[xParent] = zParent;
         }
     }
 
     long long countPairs(int n, vector<vector<int>>& edges) {
+        long long ans = 0;
         vector<int> parent(n);
-        vector<int> rank(n, 0);
         for (int i = 0; i < n; i++) {
             parent[i] = i;
         }
-
-        for (auto& a : edges) {
-            Union(a[0], a[1], parent, rank);
+        for (const auto& edge : edges) {
+            unionSets(edge[0], edge[1], parent);
         }
-
-        
-        vector<long long> component_size(n, 0);
+        vector<long long> componentSize(n, 0);
         for (int i = 0; i < n; i++) {
-            int root = findparent(i, parent);
-            component_size[root]++;
+            int root = findParent(parent, i);
+            componentSize[root]++;
         }
-
-    
-        long long ans = 0;
-        long long total_pairs = (long long)n * (n - 1) / 2;
         for (int i = 0; i < n; i++) {
-            if (component_size[i] > 0) {
-                long long size = component_size[i];
-                total_pairs -= size * (size - 1) / 2;
+            if (componentSize[i] > 0) {
+                ans += componentSize[i] * (n - componentSize[i]);
             }
         }
-
-        return total_pairs;
+        return ans / 2;
     }
 };
