2a4ec12291fa40cf4614024590b839a20eede008
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2024-09-01 15:34:43 +0530

    Time: 117 ms (45.87%) | Memory: 23.6 MB (40.68%) - LeetSync

--- 869-similar-string-groups/similar-string-groups.cpp
+++ 869-similar-string-groups/similar-string-groups.cpp
@@ -1,40 +1,46 @@
 class Solution {
 public:
-    vector<int> par, size;
-
-    int findpar(int u) {
-        return par[u] == u ? u : (par[u] = findpar(par[u]));
-    }
-
-    bool issimilar(string s1, string s2) {
-        int count = 0;
-        for (int i = 0; i < s1.length(); i++) {
-            if (s1[i] != s2[i] && ++count > 2) {
-                return false;
-            }
+    
+    bool issimilar(string &s1, string &s2) {
+        
+        int n = s1.length();
+        int diff = 0;
+        for(int i = 0; i<n; i++) {
+            if(s1[i] != s2[i])
+                diff++;
         }
-        return true;
+        
+        
+        return diff == 2 || diff == 0;
     }
-
+    void dfs(int node,unordered_map<int,vector<int>> adj,vector<bool>& vis){
+        vis[node] = true;
+        for(int a: adj[node]){
+         if(!vis[a]){
+         dfs(a,adj,vis);
+         }
+        }
+    }
     int numSimilarGroups(vector<string>& strs) {
         int n = strs.size();
-        par.resize(n); // Use resize to set the size of the vector
-        for (int i = 0; i < n; i++) {
-            par[i] = i; // Initialize each element to its own set
-        }
-        int noofset = n;
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (issimilar(strs[i], strs[j])) {
-                    int p1 = findpar(i);
-                    int p2 = findpar(j);
-                    if (p1 != p2) {
-                        par[p1] = p2; // Merge the sets
-                        noofset--;
-                    }
+        unordered_map<int,vector<int>> adj;
+        for(int i=0;i<strs.size();i++){
+            for(int j=i+1;j<strs.size();j++){
+                if(issimilar(strs[i],strs[j])){
+                adj[i].push_back(j);
+                adj[j].push_back(i);
                 }
             }
         }
-        return noofset;
+        int count =0;
+        vector<bool> vis(n,false);
+        for(int i=0;i<n;i++){
+        if(!vis[i]){
+        dfs(i,adj,vis);
+        count++;
+        }
+        }
+        return count;
+        
     }
 };
