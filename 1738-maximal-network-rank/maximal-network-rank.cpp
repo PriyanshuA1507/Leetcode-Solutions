d55c09860a1c79a34090e992ee42da9552c34d2d
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2024-09-02 14:36:38 +0530

    Time: 92 ms (25.71%) | Memory: 43 MB (35.09%) - LeetSync

--- 1738-maximal-network-rank/maximal-network-rank.cpp
+++ 1738-maximal-network-rank/maximal-network-rank.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
     int maximalNetworkRank(int n, vector<vector<int>>& roads) {
-        unordered_map<int,vector<int>> adj;
-        vector<int> rank(n,0);
-        for(auto& a:roads){
-            adj[a[0]].push_back(a[1]);
-            adj[a[1]].push_back(a[0]);
-            rank[a[0]]++;
-            rank[a[1]]++;
-        }
-        int maxi = INT_MIN;
-        for(int i=0;i<n;i++){
-          for(int j=0;j<n && j!=i;j++){
-         int rank1 = rank[i] + rank[j];
-         if (find(adj[i].begin(), adj[i].end(), j) != adj[i].end()) {
-                    rank1--; 
-                }
-        maxi = max(maxi, rank1);
-          }
-        }
-        return maxi;
+     unordered_map<int, unordered_set<int>> adj;
         
+        for(auto &road : roads) {
+            int u = road[0];
+            int v = road[1];  
+            adj[u].insert(v);
+            adj[v].insert(u);
+        }
+      int ans = INT_MIN;
+      for(int i=0;i<n;i++){
+      int total = 0;
+     for(int j=i+1;j<n;j++){
+     int a = adj[i].size();
+     int b = adj[j].size();
+      total = a+b;
+     if(adj[i].find(j)!=adj[i].end()){
+        total-=1;
+     }
+       ans = max(ans,total);
+    }
+  }  
+      return ans;
     }
 };
