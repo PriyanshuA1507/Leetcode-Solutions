a6501694bb73a1c059ef0582c2a2b2d86c6d4ac1
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2024-09-06 00:54:39 +0530

    Time: 36 ms (32.80%) | Memory: 15.9 MB (55.34%) - LeetSync

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,57 +1,52 @@
 class Solution {
 public:
-    int findTheCity(int n, vector<vector<int>>& edges, int dt) {
-        unordered_map<int,vector<pair<int,int>>> adj;
-
-        // Building the adjacency list
-        for(int i = 0; i < edges.size(); i++){
-            int u = edges[i][0]; 
-            int v = edges[i][1];
-            int w = edges[i][2];
-            adj[u].emplace_back(v, w);
-            adj[v].emplace_back(u, w);
+    int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
+        
+        vector<vector<long long>> grid(n, vector<long long>(n, INT_MAX));
+        for (int i = 0; i < n; i++) {
+            grid[i][i] = 0;
         }
 
-        vector<int> mi(n, 0);
+     
+        for (int i = 0; i < edges.size(); i++) {
+            int u = edges[i][0];
+            int v = edges[i][1];
+            int w = edges[i][2];
+            grid[u][v] = w;
+            grid[v][u] = w;
+        }
 
-        // Run BFS or Dijkstra-like algorithm for each city
-        for(int i = 0; i < n; i++) {
-            vector<int> dist(n, INT_MAX);
-            dist[i] = 0;
-            priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
-            pq.emplace(0, i);
-            
-            while(!pq.empty()){
-                auto [d, node] = pq.top();
-                pq.pop();
-                
-                if(d > dt) continue;
-                
-                for(auto& [nei, weight] : adj[node]){
-                    int new_dist = d + weight;
-                    if(new_dist <= dt && new_dist < dist[nei]){
-                        dist[nei] = new_dist;
-                        pq.emplace(new_dist, nei);
+      
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (grid[i][k] != INT_MAX && grid[k][j] != INT_MAX) {
+                        grid[i][j] = min(grid[i][j], grid[i][k] + grid[k][j]);
                     }
                 }
             }
+        }
 
-            // Count the number of reachable cities within the distance threshold
-            for(int j = 0; j < n; j++){
-                if(dist[j] <= dt) mi[i]++;
+        
+        vector<int> reachableCount(n, 0);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i != j && grid[i][j] <= distanceThreshold) {
+                    reachableCount[i]++;
+                }
             }
         }
 
-        // Find the city with the smallest number of reachable cities (and largest index in case of ties)
-        int ans = -1;
-        int mini = INT_MAX;
-        for(int i = 0; i < n; i++){
-            if(mi[i] <= mini) {
-                mini = mi[i];
-                ans = i;
+       
+        int minReachable = INT_MAX;
+        int city = -1;
+        for (int i = 0; i < n; i++) {
+            if (reachableCount[i] <= minReachable) {
+                minReachable = reachableCount[i];
+                city = i;
             }
         }
 
-        return ans;
+        return city;
     }
 };
