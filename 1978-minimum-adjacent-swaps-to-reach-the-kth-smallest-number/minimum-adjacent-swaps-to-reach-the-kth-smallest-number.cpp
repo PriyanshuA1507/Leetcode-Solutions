51db22ef492cac4f36be98d4d32f93ce624cac76
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2024-09-06 18:55:13 +0530

    Time: 21 ms (81.97%) | Memory: 8.3 MB (21.43%) - LeetSync

--- 1978-minimum-adjacent-swaps-to-reach-the-kth-smallest-number/minimum-adjacent-swaps-to-reach-the-kth-smallest-number.cpp
+++ 1978-minimum-adjacent-swaps-to-reach-the-kth-smallest-number/minimum-adjacent-swaps-to-reach-the-kth-smallest-number.cpp
@@ -1,40 +1,34 @@
-#include <string>
-#include <algorithm>
-using namespace std;
-
 class Solution {
 public:
-    string swap1(string num) {
-        int start  = num.size() - 1;
-        int end = num.size() - 2;
-        if (end >= 0) {
-            swap(num[start], num[end]);
-        }
-        return num;
-    }
-
     int getMinSwaps(string num, int k) {
-        string a = num;
-        int ans = 0;
+        string target = num;
+
+        // Generate the k-th permutation
         while (k-- > 0) {
-            next_permutation(num.begin(), num.end());
+            next_permutation(target.begin(), target.end());
         }
 
-      
-        for (int i = 0; i < a.size(); ++i) {
-            if (a[i] != num[i]) {
+        int swaps = 0;
+        string original = num;
+
+        // Process each character in the original string and compare it to the target
+        for (int i = 0; i < num.size(); i++) {
+            if (original[i] != target[i]) {
+                // Find where the target[i] is in the original string starting from i
                 int j = i;
-                while (a[j] != num[i]) {
-                    ++j;
+                while (original[j] != target[i]) {
+                    j++;
                 }
+
+                // Perform adjacent swaps to bring original[j] to position i
                 while (j > i) {
-                    swap(a[j], a[j-1]);
-                    --j;
-                    ++ans;
+                    swap(original[j], original[j - 1]);
+                    j--;
+                    swaps++;
                 }
             }
         }
-        
-        return ans;
+
+        return swaps;
     }
 };
