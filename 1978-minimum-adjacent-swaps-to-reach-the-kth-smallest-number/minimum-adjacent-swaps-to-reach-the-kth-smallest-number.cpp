8c6d8eb2a7d671eccee9585799b7b4c248058534
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2024-09-06 18:55:37 +0530

    Time: 21 ms (81.97%) | Memory: 8.3 MB (21.43%) - LeetSync

--- 1978-minimum-adjacent-swaps-to-reach-the-kth-smallest-number/minimum-adjacent-swaps-to-reach-the-kth-smallest-number.cpp
+++ 1978-minimum-adjacent-swaps-to-reach-the-kth-smallest-number/minimum-adjacent-swaps-to-reach-the-kth-smallest-number.cpp
@@ -3,7 +3,6 @@ public:
     int getMinSwaps(string num, int k) {
         string target = num;
 
-        // Generate the k-th permutation
         while (k-- > 0) {
             next_permutation(target.begin(), target.end());
         }
@@ -11,16 +10,14 @@ public:
         int swaps = 0;
         string original = num;
 
-        // Process each character in the original string and compare it to the target
         for (int i = 0; i < num.size(); i++) {
             if (original[i] != target[i]) {
-                // Find where the target[i] is in the original string starting from i
+            
                 int j = i;
                 while (original[j] != target[i]) {
                     j++;
                 }
 
-                // Perform adjacent swaps to bring original[j] to position i
                 while (j > i) {
                     swap(original[j], original[j - 1]);
                     j--;
