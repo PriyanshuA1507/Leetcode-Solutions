8936feb8ed5280ffbb73fecd103a1508a739c551
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2024-09-10 00:49:33 +0530

    Time: 60 ms (86.34%) | Memory: 20.9 MB (33.19%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
-    // Function to generate all possible words by changing one character
-    std::vector<std::string> getNextWords(const std::string& word, const std::unordered_set<std::string>& wordSet) {
-        std::vector<std::string> nextWords;
+    vector<string> check(string word,unordered_set<string>& wordSet){
+        std::vector<std::string> adjacentWords;
         std::string tempWord = word;
         for (int i = 0; i < word.size(); ++i) {
             char originalChar = tempWord[i];
@@ -10,46 +9,37 @@ public:
                 if (c == originalChar) continue;
                 tempWord[i] = c;
                 if (wordSet.find(tempWord) != wordSet.end()) {
-                    nextWords.push_back(tempWord);
+                    adjacentWords.push_back(tempWord);
                 }
             }
-            tempWord[i] = originalChar;  // Restore original character
+            tempWord[i] = originalChar;  
         }
-        return nextWords;
+        return adjacentWords;
     }
-    
-    int ladderLength(std::string beginWord, std::string endWord, std::vector<std::string>& wordList) {
-        std::unordered_set<std::string> wordSet(wordList.begin(), wordList.end());
-        
-        // Edge case: If endWord is not in wordList, return 0
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+       unordered_set<string> wordSet(wordList.begin(), wordList.end());
         if (wordSet.find(endWord) == wordSet.end()) {
             return 0;
         }
-        
-        std::queue<std::string> que;
+        queue<string> que;
+        int level= 0 ;
         que.push(beginWord);
-        int level = 1;
-        
-        while (!que.empty()) {
-            int size = que.size();
-            for (int i = 0; i < size; ++i) {
-                std::string word = que.front();
-                que.pop();
-                
-                if (word == endWord) {
-                    return level;
-                }
-                
-                // Generate all possible next words
-                std::vector<std::string> nextWords = getNextWords(word, wordSet);
-                for (const std::string& nextWord : nextWords) {
-                    wordSet.erase(nextWord);  // Mark this word as visited
-                    que.push(nextWord);
-                }
+        while(que.size()!=0){
+         int size = que.size();
+         for(int i=0;i<size;i++){
+            string a = que.front();
+            que.pop();
+            if(a==endWord){
+                return level+1;
             }
-            ++level;
+            vector<string> b = check(a,wordSet);
+            for(auto a1:b){
+                wordSet.erase(a1);
+                    que.push(a1);
+            }
+         }
+         level++;
         }
-        
         return 0;
     }
 };
