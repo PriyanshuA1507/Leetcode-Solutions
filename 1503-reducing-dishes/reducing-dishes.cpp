9fd4fae071e06e4469d98ea4c16985c2dfc2b298
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2024-09-12 22:25:37 +0530

    Time: 36 ms (6.12%) | Memory: 32.7 MB (9.23%) - LeetSync

--- 1503-reducing-dishes/reducing-dishes.cpp
+++ 1503-reducing-dishes/reducing-dishes.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
-    int solve(vector<int>& satisfaction,int index,int time,vector<vector<int>>& dp){
-        if(index == satisfaction.size()){
+    int solve(vector<int>& s,int index,int time, vector<vector<int>>& dp){
+        if(index>=s.size()){
             return 0;
         }
         if(dp[index][time]!=-1){
             return dp[index][time];
         }
-        int include = satisfaction[index]*(time+1)+ solve(satisfaction,index+1,time+1,dp);
-       
-       int exclude = 0 + solve(satisfaction,index+1,time,dp);
-        
-          dp[index][time] = max(include,exclude);
-        return dp[index][time];
-
+        int take = s[index]*time + solve(s,index+1,time+1,dp);
+        int nottake = solve(s,index+1,time,dp);
+        return dp[index][time]=max(take,nottake);
     }
-    int maxSatisfaction(vector<int>& satisfaction) {
-        int n = satisfaction.size();
-        vector<vector<int>> dp(n, vector<int>(n,-1));
-        sort(satisfaction.begin(),satisfaction.end());
-        return solve(satisfaction,0,0,dp);
+    int maxSatisfaction(vector<int>& s) {
+        int n = s.size();
+        vector<vector<int>> dp(500,vector<int>(n+1,-1));
+        sort(s.begin(),s.end());
+        return solve(s,0,1,dp);
     }
 };
