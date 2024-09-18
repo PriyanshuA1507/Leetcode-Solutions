d22ef15fad7bb34a8cf81569ba211f26e2681e9f
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2024-09-19 01:54:12 +0530

    Time: 5 ms (20.61%) | Memory: 7.4 MB (50.08%) - LeetSync

---
+++ 397-integer-replacement/integer-replacement.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    long long solve(long long n) {
+        if (n == 1) {
+            return 0;
+        }
+        if (n % 2 == 0) {
+            return 1 + solve(n / 2);  
+        } else {
+           
+            return 1 + min(solve(n + 1), solve(n - 1));
+        }
+    }
+
+    int integerReplacement(int n) {
+        return solve(static_cast<long long>(n));  
+    }
+};
