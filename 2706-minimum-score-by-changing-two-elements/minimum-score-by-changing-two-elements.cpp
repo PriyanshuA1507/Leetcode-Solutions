7479b199ff244aca5a246d47827279cfb035d2d4
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2024-09-19 12:21:25 +0530

    Time: 64 ms (58.92%) | Memory: 37.9 MB (65.44%) - LeetSync

--- 2706-minimum-score-by-changing-two-elements/minimum-score-by-changing-two-elements.cpp
+++ 2706-minimum-score-by-changing-two-elements/minimum-score-by-changing-two-elements.cpp
@@ -1,21 +1,15 @@
 class Solution {
 public:
     int minimizeSum(vector<int>& nums) {
-        // Sort the array
-        sort(nums.begin(), nums.end());
-        
-        int n = nums.size();
-        
-        // Case 1: Change the two largest values
-        int option1 = nums[n-3] - nums[0];
-        
-        // Case 2: Change the largest and the smallest value
-        int option2 = nums[n-2] - nums[1];
-        
-        // Case 3: Change the two smallest values
-        int option3 = nums[n-1] - nums[2];
-        
-        // The minimized sum is the minimum of the three options
-        return min({option1, option2, option3});
+   int n = nums.size();
+    sort(nums.begin(),nums.end());
+    //1. change two largest value
+    //2. changt two smallest value
+    //3. change one largest and one smallest value
+    int option1 = nums[n-3]-nums[0];
+    int option2 = nums[n-1]-nums[2];
+    int option3 = nums[n-2]-nums[1];
+
+    return min({option1,option2,option3});
     }
 };
