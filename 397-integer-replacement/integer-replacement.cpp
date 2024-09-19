a5a16e747b2dd2cbe5987e114cca80fcfeec495d
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2024-09-19 11:22:06 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (10.00%) - LeetSync

--- 397-integer-replacement/integer-replacement.cpp
+++ 397-integer-replacement/integer-replacement.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
+   unordered_map<int,int> dp;
     long long solve(long long n) {
         if (n == 1) {
             return 0;
         }
+        if(dp.find(n)!=dp.end()){
+            return dp[n];
+        }
         if (n % 2 == 0) {
-            return 1 + solve(n / 2);  
+           return dp[n]= 1 + solve(n / 2);  
         } else {
            
-            return 1 + min(solve(n + 1), solve(n - 1));
+            return dp[n]=1 + min(solve(n + 1), solve(n - 1));
         }
     }
 
