d8827271be68177ca061952b4ae4b434fae75249
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2024-09-20 18:23:14 +0530

    Time: 11 ms (25.53%) | Memory: 31 MB (94.89%) - LeetSync

--- 2753-minimum-number-of-operations-to-make-all-array-elements-equal-to-1/minimum-number-of-operations-to-make-all-array-elements-equal-to-1.cpp
+++ 2753-minimum-number-of-operations-to-make-all-array-elements-equal-to-1/minimum-number-of-operations-to-make-all-array-elements-equal-to-1.cpp
@@ -1,36 +1,27 @@
-#include <vector>
-#include <numeric>  // For std::gcd
-#include <algorithm>  // For std::sort
-using namespace std;
-
 class Solution {
 public:
     int minOperations(vector<int>& nums) {
         int n = nums.size();
-        int minOps = n;  // Initialize with maximum possible operations
+        int minOps = n; 
         if (count(nums.begin(), nums.end(), 1) > 0) {
          return (n-count(nums.begin(), nums.end(), 1));
         }
-        // Try to find the minimum subarray with GCD 1
+
         for (int i = 0; i < n; ++i) {
             int gcd_result = nums[i];
             for (int j = i + 1; j < n; ++j) {
                 gcd_result = __gcd(gcd_result, nums[j]);
-                
-                // If we found a subarray with GCD 1, calculate operations
+            
                 if (gcd_result == 1) {
-                    minOps = min(minOps, j - i);  // Calculate minimal operations for this subarray
+                    minOps = min(minOps, j - i);  
                     break;
                 }
             }
         }
         
-        // If no subarray with GCD 1 is found, return -1
         if (minOps == n) {
             return -1;
         }
-
-        // Total operations: operations to turn the subarray into 1, and the rest of the array
         return minOps + (n - 1);
     }
 };
