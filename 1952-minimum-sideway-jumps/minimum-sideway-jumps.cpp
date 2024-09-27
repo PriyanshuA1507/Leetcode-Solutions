fbc2da00b4ab8ce57fdf6d64f7c03dfbc4c33ef7
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2024-09-27 17:57:45 +0530

    Time: 501 ms (14.10%) | Memory: 360.9 MB (16.37%) - LeetSync

--- 1952-minimum-sideway-jumps/minimum-sideway-jumps.cpp
+++ 1952-minimum-sideway-jumps/minimum-sideway-jumps.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
-   int solve(int index,vector<int>& obstacles,int lane,vector<vector<int>>& dp){
-    if(index==obstacles.size()-1){
-        return 0;
-    }
-    if(dp[index][lane]!=-1){
-        return dp[index][lane];
-    }
-    if(obstacles[index+1]!=lane){
-     return solve(index+1,obstacles,lane,dp);
-    }
-    int ans = INT_MAX;
-    for(int i=1;i<=3;i++){
-        if(lane!= i && obstacles[index]!=i){
-            ans = min(ans,  1+solve(index,obstacles,i,dp));
+    int solve(vector<int>& o,int currpos,int currlane,vector<vector<int>>& dp){
+        int n = o.size();
+        if(currpos==n-1){
+            return 0;
         }
+        if(dp[currpos][currlane]!=-1){
+            return dp[currpos][currlane];
+        }
+        if(o[currpos+1]!=currlane){
+            return solve(o,currpos+1,currlane,dp);
+        }
+    
+        int ans = INT_MAX;
+        for(int i=1;i<=3;i++){
+        if(currlane!=i && o[currpos]!=i){
+        ans = min(ans, 1+solve(o,currpos,i,dp));
+        }
+        }
+        return dp[currpos][currlane]=ans;
     }
-     dp[index][lane] = ans;
-     return dp[index][lane];
-   }
     int minSideJumps(vector<int>& obstacles) {
-        int n = obstacles.size();
-     vector<vector<int>> dp(n,vector<int>(4,-1));
-     return solve(0,obstacles,2,dp);
+    int n = obstacles.size(); 
+    vector<vector<int>> dp(n+1,vector<int>(4,-1));
+    return solve(obstacles,0,2,dp);
     }
 };
