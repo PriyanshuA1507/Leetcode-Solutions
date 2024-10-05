4143ef62d9f5218233c007fc818c2939e4615b25
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2024-10-05 09:31:47 +0530

    Time: 111 ms (94.43%) | Memory: 55.9 MB (88.87%) - LeetSync

--- 148-sort-list/sort-list.cpp
+++ 148-sort-list/sort-list.cpp
@@ -10,30 +10,55 @@
  */
 class Solution {
 public:
+    // Function to split the list into two halves
+    ListNode* getMid(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+        
+        while(fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        
+        ListNode* mid = slow->next;
+        slow->next = nullptr;
+        return mid;
+    }
+
+    // Function to merge two sorted lists
+    ListNode* merge(ListNode* left, ListNode* right) {
+        ListNode dummy;
+        ListNode* tail = &dummy;
+
+        while(left && right) {
+            if(left->val < right->val) {
+                tail->next = left;
+                left = left->next;
+            } else {
+                tail->next = right;
+                right = right->next;
+            }
+            tail = tail->next;
+        }
+
+        if(left) tail->next = left;
+        if(right) tail->next = right;
+
+        return dummy.next;
+    }
+
+    // Function to sort the linked list using merge sort
     ListNode* sortList(ListNode* head) {
-         ListNode* temp = head;
-
-        vector<int> v;
-
-        while(temp != NULL)
-        {
-            v.push_back(temp-> val);
-            temp = temp-> next;
+        if(!head || !head->next) {
+            return head;
         }
 
-        sort(v.begin(),v.end());
+        // Split the list into two halves
+        ListNode* mid = getMid(head);
+        ListNode* left = sortList(head);
+        ListNode* right = sortList(mid);
 
-        ListNode* newHead = new ListNode(0);
-        temp = newHead;
-
-        for(int num : v)
-        {
-            temp-> next = new ListNode(num);
-            temp = temp-> next;
-        }
-        ListNode* result = newHead-> next;
-        delete newHead;
-
-        return result;
+        // Merge the two sorted halves
+        return merge(left, right);
     }
 };
