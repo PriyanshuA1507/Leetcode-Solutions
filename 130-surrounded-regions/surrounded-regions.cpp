f3efa9d3e233f6ee6f26e81e38b2beb5f0685092
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2024-10-13 08:51:31 +0530

    Time: 16 ms (14.48%) | Memory: 14.5 MB (24.00%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.cpp
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -1,38 +1,44 @@
 class Solution {
 public:
-  void dfs(vector<vector<char>> &board,int i,int j,vector<vector<int>> &dir){
-    board[i][j]='$';
-    int n= board.size() , m= board[0].size();
-    for(int d=0;d<dir.size();d++){
-        int r = i+ dir[d][0];
-        int c= j + dir[d][1];
-    if(r>=0 && c>=0 && r<n && c<m && board[r][c]=='O'){
-       
-        dfs(board,r,c,dir);
-    }
-
-    }
-
-  }
+   using p = pair<int,int>;
+   vector<vector<int>> dirs = {{-1,0},{0,1},{1,0},{0,-1}};
     void solve(vector<vector<char>>& board) {
-       vector<vector<int>> dir={{-1,0},{0,1},{1,0},{0,-1}};
-       int n= board.size() , m= board[0].size();
-       for(int i=0;i<n;i++){
-           for(int j=0;j<m;j++){
-               if(i==0 || j==0 || i==n-1 || j==m-1){
-                   if(board[i][j]=='O'){
-                   dfs(board,i,j,dir);}
-               }
-           }
-       }
-       for(int i=0;i<n;i++){
-           for(int j=0;j<m;j++){
-               if(board[i][j]=='$'){
-                   board[i][j]='O';
-               }
-               else board[i][j]='X';
-           }
-       }
+        int n = board.size();
+        int m = board[0].size();
+        queue<p>q;
+        for(int i=0;i<board.size();i++){
+        for(int j=0;j<board[0].size();j++){
+         if(board[i][j]=='O' && (i==0 || i==n-1 || j==0 || j==m-1)){
+            q.push({i,j});
+            board[i][j]='c';
+         }
+        }
+        }
+        while(q.size()!=0){
+        int s = q.size();
+        for(int i=0;i<s;i++){
+        auto a = q.front();
+        q.pop();
+        for(auto d: dirs){
+        int r = a.first + d[0];
+        int c = a.second + d[1];
 
+        if(r>=0 && c>=0 && r<n && c<m && board[r][c]=='O'){
+            board[r][c] = 'c';
+            q.push({r,c});
+        }
+        }
+        }
+        }
+        for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+        if(board[i][j]=='O' ){
+            board[i][j]='X';
+        }
+        if (board[i][j] == 'c') {
+                    board[i][j] = 'O'; 
+                }
+        }
+        }
     }
 };
