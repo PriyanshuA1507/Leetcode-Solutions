81d0412c4a49bae92727606660599040e9fc2b0a
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2024-10-14 14:17:53 +0530

    Time: 290 ms (5.07%) | Memory: 82.7 MB (6.27%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -1,73 +1,38 @@
 class Solution {
 public:
-    vector<vector<int>> dirs = {{-1,0}, {-1,-1}, {0,1}, {1,1}, {1,0}, {1,-1}, {0,-1}, {-1,1}};
-    
-   /* int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        int n = grid.size();
-        if (grid[0][0] == 1 || grid[n-1][n-1] == 1) {
+    using p = pair<int,int>;
+    vector<vector<int>> dirs = {{-1,0},{-1,1},{0,1},{1,1},{1,0},{1,-1},{0,-1},{-1,-1}};
+    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+    int n = grid.size();
+    if(grid[0][0] == 1 || grid[n-1][n-1] == 1) {
             return -1;
         }
-        
-        vector<vector<int>> visited(n, vector<int>(n, 0));
-        queue<pair<int, int>> q;
-        q.push({0, 0});
-        visited[0][0] = 1;
-        
-        int pathLength = 1; // Start from the first cell
-        
-        while (!q.empty()) {
-            int size = q.size();
-            for (int i = 0; i < size; ++i) {
-                auto [r, c] = q.front();
-                q.pop();
-                
-                if (r == n-1 && c == n-1) {
-                    return pathLength;
-                }
-                
-                for (auto& d : dirs) {
-                    int nr = r + d[0];
-                    int nc = c + d[1];
-                    
-                    if (nr >= 0 && nc >= 0 && nr < n && nc < n && grid[nr][nc] == 0 && !visited[nr][nc]) {
-                        q.push({nr, nc});
-                        visited[nr][nc] = 1;
-                    }
-                }
-            }
-            ++pathLength;
-        }
-        
-        return -1; // If we reach here, there's no path
-    }*/
-    typedef pair<int,pair<int,int>> pp;
-     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        int n = grid.size();
-        if (grid[0][0] == 1 || grid[n-1][n-1] == 1) {
-            return -1;
-        }
-    priority_queue<pp ,vector<pp>, greater<pp>>pq;
-    vector<vector<int>> result(n+1,vector<int>(n+1,INT_MAX));
-    result[0][0]=0;
-    pq.push({0,{0,0}});
-    while(pq.size()!=0){
-     auto a = pq.top();
-     pq.pop();
-     pair<int,int>p = a.second;
-     int dist = a.first;
-     for(auto b: dirs){
-     int nr = p.first + b[0];
-     int nc = p.second + b[1];
-     int dist1 = 1;
-  if (nr >= 0 && nc >= 0 && nr < n && nc < n && grid[nr][nc] == 0 && result[nr][nc]>dist1+dist) {
-                   pq.push({dist+dist1,{nr, nc}});
-               result[nr][nc]=dist1+dist;
-        }
-     }
+    vector<vector<bool>> visited(n,vector<bool>(n,false));
+    queue<p> q;
+    q.push({0,0});
+    int pathlength=1;
+    visited[0][0] = true;
+    while(q.size()!=0){
+    int size = q.size();
+    for(int i=0;i<size;i++){
+    auto a = q.front();
+    int j = a.first;
+    int k = a.second;
+    q.pop();
+    if(j==n-1 && k==n-1){
+        return pathlength;
     }
-  if(result[n-1][n-1]==INT_MAX){
-    return -1;
-  }
-  return result[n-1][n-1]+1;
-     }
+    for(auto d:dirs){
+    int r = j + d[0];
+    int c = k + d[1];
+    if(r>=0 && c>=0 && r<n && c<n && !visited[r][c] && grid[r][c]==0){
+        visited[r][c] = true;
+        q.push({r,c});
+    }
+    }
+    }
+    pathlength++;
+    }
+     return -1;
+    }
 };
