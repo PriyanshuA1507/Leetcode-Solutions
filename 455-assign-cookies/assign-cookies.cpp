7cc5248571d57753c49219a1c6a94c9ddbb11707
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2024-10-28 12:07:57 +0530

    Time: 13 ms (27.53%) | Memory: 44.8 MB (73.66%) - LeetSync

--- 455-assign-cookies/assign-cookies.cpp
+++ 455-assign-cookies/assign-cookies.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
-        std::sort(g.begin(), g.end());
-        std::sort(s.begin(), s.end());
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
 
         int ans = 0;
         int childIndex = 0;
