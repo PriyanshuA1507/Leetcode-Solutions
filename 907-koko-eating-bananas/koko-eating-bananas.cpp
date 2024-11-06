cbc42ea765112166d6e571eea9e901f23fab1183
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2024-11-06 11:20:28 +0530

    Time: 8 ms (63.66%) | Memory: 21.7 MB (78.12%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,36 +1,29 @@
 class Solution {
 public:
-   bool possible(vector<int>& piles, long h,long mid){
-   long ans = 0;
-   for(int i=0;i<piles.size();i++){
-    if(mid!=0 && piles[i]%mid==0){
-        ans+=(piles[i]/mid);
+    bool check(int mid, vector<int>& piles, int h) {
+        long long ans = 0;
+        for (int i = 0; i < piles.size(); i++) {
+            ans += (piles[i] + mid - 1) / mid;  
+        }
+        return ans <= h;  
     }
-    else if(mid!=0 && piles[i]%mid!=0){
-        ans+=(piles[i]/mid +1);
-    }
-   }
-   if(h>=ans){
-    return true;
-   }
-   return false;
-   }
+
     int minEatingSpeed(vector<int>& piles, int h) {
-    int n = piles.size();
-     sort(piles.begin(),piles.end());
-     long left = 1;
-     long right = piles[n-1];
-     long ans = 0;
-     while(left<=right){
-      long mid = (left+right)/2;
-      if(possible(piles,h,mid)){
-        ans = mid;
-        right = mid-1;
-      }
-      else{
-        left = mid+1;
-      }
-     }
-     return ans;   
+        int ma = *max_element(piles.begin(), piles.end());
+        int start = 1;
+        int end = ma;
+        int ans = -1;
+        
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (check(mid, piles, h)) {
+                ans = mid;
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        
+        return ans;
     }
 };
