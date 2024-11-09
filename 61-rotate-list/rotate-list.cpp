251936711abe5923afae3fc4f342f83b18383556
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2024-11-09 11:58:51 +0530

    Time: 0 ms (100.00%) | Memory: 16.5 MB (92.63%) - LeetSync

--- 61-rotate-list/rotate-list.cpp
+++ 61-rotate-list/rotate-list.cpp
@@ -1,32 +1,33 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
-        if(!head){return head;}
-       int size=1;
-       ListNode* a = head;
-       while(a->next){
-        a=a->next;
-        size++;
-       }
-       k=k%size;
-       a->next = head;
-       a = head;
-       for(int i=1;i<size-k;i++){
-          a = a->next;
-       }
-       head= a->next;
-        a->next= NULL;
-        return head;
-    }
+        if (!head || !head->next || k == 0) return head;
 
+
+        int length = 1;
+        ListNode* temp = head;
+        while (temp->next) {
+            length++;
+            temp = temp->next;
+        }
+
+        // Connect the last node to the head to make it circular
+        temp->next = head;
+
+        // Update k to avoid unnecessary rotations
+        k = k % length;
+        int stepsToNewHead = length - k;
+
+        // Find the new tail (the node before the new head)
+        temp = head;
+        for (int i = 0; i < stepsToNewHead - 1; i++) {
+            temp = temp->next;
+        }
+
+        // Set the new head and break the loop
+        ListNode* newHead = temp->next;
+        temp->next = nullptr;
+
+        return newHead;
+    }
 };
