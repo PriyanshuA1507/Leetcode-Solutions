257f333c377c823526f624a4c2255aa173f1d402
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2024-11-09 10:45:08 +0530

    Time: 33 ms (84.38%) | Memory: 17.2 MB (96.16%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -18,11 +18,14 @@ public:
 
         // Traverse both lists. When reaching the end of one list, continue from the beginning of the other.
         while (a != b) {
+ // If `a` reaches the end of list A, switch it to the start of list B.
             a = (a == nullptr) ? headB : a->next;
+ // If `b` reaches the end of list B, switch it to the start of list A.
             b = (b == nullptr) ? headA : b->next;
         }
 
-        // Either intersection node or nullptr if no intersection.
+      // If lists intersect, `a` and `b` will eventually meet at the intersection node.
+     // If no intersection, both `a` and `b` will reach nullptr simultaneously.
         return a;
     }
 };
