cc2c5a0495052261699f176258cea98d4e9c2eb0
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2024-11-15 09:39:49 +0530

    Time: 7 ms (40.73%) | Memory: 17.8 MB (47.60%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,38 +1,36 @@
 class Solution {
 public:
-    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<int> result;
-        queue<int> q;
-        int c=0;
-        unordered_map<int,vector<int>> adj;
-        vector<int> indegree(numCourses,0);
-        for(auto a: prerequisites){
-        int u = a[0], v = a[1];
-        adj[u].push_back(v);
-        indegree[v]++;
-        }
-        for(int i=0;i<numCourses;i++){
-            if(indegree[i]==0){
-                q.push(i);
-                c++;
-            }
-        }
-        while(q.size()!=0){
-        int a = q.front();
-        result.push_back(a);
-        q.pop();
-        for(auto v: adj[a]){
-         indegree[v]--;
-         if(indegree[v]==0){
-        c++;
-         q.push(v);
-         }
-        }
-        }
-        reverse(result.begin(),result.end());
-        if(c==numCourses){
-        return result;
-        }
-        return {};
+    vector<int> findOrder(int numCourses, vector<vector<int>>& pr) {
+        unordered_map<int,vector<int>> m;
+     queue<int> q;
+     vector<int> result;
+     vector<int> indegree(numCourses,0);
+     for(int i=0;i<pr.size();i++){
+    int u = pr[i][0];
+    int v = pr[i][1];
+    m[u].push_back(v);
+    indegree[v]++;
+     }
+   for(int i=0;i<numCourses;i++){
+    if(indegree[i]==0){
+     q.push(i);
+    }
+   }
+   while(q.size()!=0){
+    int a = q.front();
+    result.push_back(a);
+    q.pop();
+    for(auto b: m[a]){
+    indegree[b]--;
+    if(indegree[b]==0){
+        q.push(b);
+    }
+    }
+   }
+   reverse(result.begin(),result.end());
+   if(result.size()==numCourses){
+    return result;
+   }
+   return {}; 
     }
 };
