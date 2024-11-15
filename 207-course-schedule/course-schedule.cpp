1af1c0fb4d0cd5cb20061cd9610eab6aaf5da9ff
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2024-11-15 09:38:04 +0530

    Time: 3 ms (80.73%) | Memory: 19.5 MB (44.11%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,35 +1,35 @@
 class Solution {
 public:
-    bool canFinish(int numCourses, vector<vector<int>>& p) {
-       vector<int> indegree(numCourses,0);
-       queue<int> q;
-       unordered_map<int,vector<int>> adj;
-       for(auto a: p){
-      int u = a[0], v = a[1];
-       adj[u].push_back(v);
-       indegree[v]++;
-       }
-       int c=0;
-       for(int i=0;i<numCourses;i++){
-        if(indegree[i]==0){
-        c++;
-         q.push(i);
-        }
-       }
-       while(q.size()!=0){
-      int u = q.front();
-       q.pop();
-       for(auto v: adj[u]){
-      indegree[v]--;
-      if(indegree[v]==0){
-        c++;
-        q.push(v);
-      }
-       }
-       }
-       if(c==numCourses){
-        return true;
-       }
-       return false;
+    bool canFinish(int numCourses, vector<vector<int>>& pr) {
+     unordered_map<int,vector<int>> m;
+     queue<int> q;
+     vector<int> result;
+     vector<int> indegree(numCourses,0);
+     for(int i=0;i<pr.size();i++){
+    int u = pr[i][0];
+    int v = pr[i][1];
+    m[u].push_back(v);
+    indegree[v]++;
+     }
+   for(int i=0;i<numCourses;i++){
+    if(indegree[i]==0){
+     q.push(i);
+    }
+   }
+   while(q.size()!=0){
+    int a = q.front();
+    result.push_back(a);
+    q.pop();
+    for(auto b: m[a]){
+    indegree[b]--;
+    if(indegree[b]==0){
+        q.push(b);
+    }
+    }
+   }
+   if(result.size()==numCourses){
+    return true;
+   }
+   return false;
     }
 };
