159634a6e1fab04faa9c0e7496bd3efec07cc8e0
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2024-11-15 17:46:22 +0530

    Time: 6 ms (10.20%) | Memory: 15.1 MB (5.38%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -1,33 +1,28 @@
 class Solution {
 public:
-    int solve(vector<vector<int>>& matrix, int sr, int sc, vector<vector<int>>& memo) {
-        int row = matrix.size();
-        int col = matrix[0].size();
-        
-        if (sr == row - 1 && sc >= 0 && sc < col) {
-            return matrix[sr][sc];
-        }
-        if (sr >= row || sc < 0 || sc >= col) {
-            return 1e9;
-        }
-        if (memo[sr][sc] != INT_MAX) {
-            return memo[sr][sc];
-        }
-
-        int o1 = matrix[sr][sc] + solve(matrix, sr + 1, sc - 1, memo);
-        int o2 = matrix[sr][sc] + solve(matrix, sr + 1, sc, memo);
-        int o3 = matrix[sr][sc] + solve(matrix, sr + 1, sc + 1, memo);
-
-        return memo[sr][sc] = min({o1, o2, o3});
+    int solve(vector<vector<int>>& matrix,int sr,int sc, vector<vector<int>>& dp){
+    int row = matrix.size();
+    int col = matrix[0].size();
+    if(sr==row-1 && sc>=0 &&sc<col){
+      return matrix[sr][sc];
     }
-
+    if(sr>=row || sc>=col || sc<0){
+        return 1e9;
+    }
+    if(dp[sr][sc]!=INT_MAX){
+        return dp[sr][sc];
+    }
+    int o1 = matrix[sr][sc] + solve(matrix,sr+1,sc-1,dp);
+    int o2 = matrix[sr][sc] + solve(matrix,sr+1,sc,dp);
+    int o3 = matrix[sr][sc] + solve(matrix,sr+1,sc+1,dp);
+    return dp[sr][sc]=min({o1,o2,o3});
+    }
     int minFallingPathSum(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        vector<vector<int>> dp(n+1,vector<int>(n+1,INT_MAX));
         int ans = INT_MAX;
-        int col = matrix[0].size();
-        vector<vector<int>> memo(matrix.size(), vector<int>(col, INT_MAX));
-
-        for (int i = 0; i < col; i++) {
-            ans = min(ans, solve(matrix, 0, i, memo));
+        for(int i=0;i<matrix[0].size();i++){
+        ans = min(ans,solve(matrix,0,i,dp));
         }
         return ans;
     }
