1e994c6782d70bbcad8582255abf0ecc72ae87be
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2024-11-16 14:13:40 +0530

    Time: 12 ms (69.11%) | Memory: 16.4 MB (26.86%) - LeetSync

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,48 +1,38 @@
 class Solution {
 public:
     int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
-        
-        vector<vector<long long>> grid(n, vector<long long>(n, INT_MAX));
-        for (int i = 0; i < n; i++) {
+        vector<vector<long long>> grid(n, vector<long long>(n,INT_MAX));
+        for(int i=0;i<n;i++){
             grid[i][i] = 0;
         }
-
+        for(auto a:edges){
+        int u = a[0];
+        int v = a[1];
+        int w = a[2];
+        grid[u][v] = w;
+        grid[v][u] = w;
+        }
+        for(int via = 0; via<n;via++){
+        for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+        grid[i][j] = min(grid[i][j], grid[i][via] + grid[via][j]);
+        }
+        }
+        }
+     vector<int> viscount(n,0);
+     for(int i=0;i<n;i++){
+    for(int j=0;j<n;j++){
+    if(i!=j && grid[i][j]<=distanceThreshold){
+       viscount[i]++; 
+    }
+    }
+     }
      
-        for (int i = 0; i < edges.size(); i++) {
-            int u = edges[i][0];
-            int v = edges[i][1];
-            int w = edges[i][2];
-            grid[u][v] = w;
-            grid[v][u] = w;
-        }
-
-      
-        for (int k = 0; k < n; k++) {
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    if (grid[i][k] != INT_MAX && grid[k][j] != INT_MAX) {
-                        grid[i][j] = min(grid[i][j], grid[i][k] + grid[k][j]);
-                    }
-                }
-            }
-        }
-
-        
-        vector<int> reachableCount(n, 0);
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i != j && grid[i][j] <= distanceThreshold) {
-                    reachableCount[i]++;
-                }
-            }
-        }
-
-       
-        int minReachable = INT_MAX;
+     int minCount = INT_MAX;
         int city = -1;
         for (int i = 0; i < n; i++) {
-            if (reachableCount[i] <= minReachable) {
-                minReachable = reachableCount[i];
+            if (viscount[i] < minCount || (viscount[i] == minCount && i > city)) {
+                minCount = viscount[i];
                 city = i;
             }
         }
