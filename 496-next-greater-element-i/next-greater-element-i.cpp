57d8a16f13f5b686f7b672706c3aa04738606a8b
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2024-11-27 02:27:40 +0530

    Time: 4 ms (29.57%) | Memory: 12.6 MB (7.19%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> ans2;
         unordered_map<int,int> m;
         for(int i=0;i<nums2.size();i++){
-            m[nums2[i]]=i;
+            m[nums2[i]] = i;
         }
         for(int i=0;i<nums1.size();i++){
-            int a = nums1[i];
-            bool c = false;
-         for(int j = m[a];j<nums2.size();j++){
-            if(a<nums2[j]){
-                nums1[i]=nums2[j];
-                c = true;
-                break;
-            }
-         } 
-         if(!c){
-            nums1[i] = -1;
+        int j = m[nums1[i]];
+        int ans = -1;
+        for(int k=j;k<nums2.size();k++){
+         if(nums2[k]>nums1[i]){
+            ans = nums2[k];
+            break;
          }
         }
-        return nums1;
-        
+        ans2.push_back(ans);
+        }
+        return ans2;
     }
 };
