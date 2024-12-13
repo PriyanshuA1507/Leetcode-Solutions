70d1908bef767684770c1abd662ad51d74b543da
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2024-12-13 21:44:10 +0530

    Time: 0 ms (100.00%) | Memory: 28.2 MB (5.82%) - LeetSync

--- 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.cpp
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.cpp
@@ -1,35 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    
-    int maxSum;
-    
-    int solve(TreeNode* root) {
-        if(root == NULL)
-            return 0;
-        
-        int l = solve(root->left);
-        int r = solve(root->right);
-        
-        int neeche_hi_milgaya_answer = l + r + root->val; //(1)
-        
-        int koi_ek_acha = max(l, r) + root->val; //(2)
-        
-        int only_root_acha = root->val; //(3)
-        
-        maxSum = max({maxSum, neeche_hi_milgaya_answer, koi_ek_acha, only_root_acha});
-        
-        
-        //most important part
-        return max(koi_ek_acha, only_root_acha);
-        
+    int maxsum;
+    int solve(TreeNode* root){
+    if(!root){
+        return 0;
     }
-    
+    int l = solve(root->left);
+    int r = solve(root->right);
+
+    int neeche_hi_milgya_answer = l+ r + root->val;
+     int koi_ek_acha = max(l,r) + root->val;
+     int only_root_acha = root->val;
+     maxsum = max({maxsum,neeche_hi_milgya_answer,koi_ek_acha,only_root_acha});
+
+     return max({koi_ek_acha,only_root_acha});
+    }
     int maxPathSum(TreeNode* root) {
-        maxSum = INT_MIN;
-        
-        solve(root);
-        
-        return maxSum;
-        
+       maxsum = INT_MIN;
+      solve(root);
+       return maxsum; 
     }
 };
