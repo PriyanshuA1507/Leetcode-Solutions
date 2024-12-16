34e3c3126c5b330d80a7e68bfba0d76aa7af6a34
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2024-12-16 12:28:00 +0530

    Time: 33 ms (12.99%) | Memory: 22.5 MB (23.06%) - LeetSync

--- 690-employee-importance/employee-importance.cpp
+++ 690-employee-importance/employee-importance.cpp
@@ -1,39 +1,47 @@
-
+/*
+// Definition for Employee.
+class Employee {
+public:
+    int id;
+    int importance;
+    vector<int> subordinates;
+};
+*/
 
 class Solution {
 public:
     int getImportance(vector<Employee*> employees, int id) {
-        unordered_map<int, Employee*> employeeMap;
-        for (auto e : employees) {
-            employeeMap[e->id] = e;
+         unordered_map<int, bool> visited;
+        unordered_map<int,int> m;
+        for(auto a:employees){
+         int id1 = a->id;
+         int importance2 = a->importance;
+         m[id1] = importance2;
         }
-
-        unordered_map<int, vector<int>> adj;
-        for (auto a : employees) {
-            int id1 = a->id;
-            vector<int> b = a->subordinates;
-            for (auto c : b) {
-                adj[id1].push_back(c);
-            }
+        unordered_map<int,vector<int>> adj;
+        for(auto a: employees){
+         int id1 = a->id;
+         vector<int> b = a->subordinates;
+         for(auto c:b){
+          adj[id1].push_back(c);
+         }
         }
-
-        unordered_map<int, bool> visited;
         int sum = 0;
         queue<int> q;
         q.push(id);
         visited[id] = true;
-
-        while (!q.empty()) {
-            int id2 = q.front();
-            q.pop();
-            sum += employeeMap[id2]->importance;
-            for (auto a : adj[id2]) {
-                if (!visited[a]) {
-                    visited[a] = true;
-                    q.push(a);
-                }
+        while(q.size()!=0){
+         int id2 = q.front();
+         q.pop();
+         sum+=m[id2];
+         for(auto a: adj[id2]){
+            if(!visited[a]){
+             visited[a] = true;
+             q.push(a);
             }
+         }
         }
         return sum;
+
     }
 };
