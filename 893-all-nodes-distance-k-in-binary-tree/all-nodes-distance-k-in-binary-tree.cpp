d704cf8f3a0fde7be79a0f9f3fd46c18ffb3a285
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2024-12-20 18:02:52 +0530

    Time: 9 ms (19.91%) | Memory: 15.6 MB (39.76%) - LeetSync

--- 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.cpp
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.cpp
@@ -1,60 +1,52 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
 class Solution {
 public:
-    void buildgraph(TreeNode* root,unordered_map<int,vector<int>>&adj){
-       if(!root){
-           return;
-       }
-      if(root->left){
-     adj[root->val].emplace_back(root->left->val);
-     adj[root->left->val].emplace_back(root->val);
-     buildgraph(root->left,adj);
-      }
-      if(root->right){
-     adj[root->val].emplace_back(root->right->val);
-     adj[root->right->val].emplace_back(root->val);
-     buildgraph(root->right,adj);
-      }
-      
-   }
+    void buildgraph(TreeNode* root, unordered_map<int, vector<int>>& adj) { 
+        if (!root) {
+            return;
+        }
+        if (root->left) {
+            adj[root->val].emplace_back(root->left->val);
+            adj[root->left->val].emplace_back(root->val);
+            buildgraph(root->left, adj);
+        }
+        if (root->right) {
+            adj[root->val].emplace_back(root->right->val);
+            adj[root->right->val].emplace_back(root->val);
+            buildgraph(root->right, adj);
+        }
+    }
+    
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
-     if(!root){
-        return {};
-     }
-       unordered_map<int,vector<int>>adj;
-       buildgraph(root,adj);
-       unordered_map<int, bool> vis;
-       vis[target->val] = true;
-       queue<int> q;
-       q.push(target->val);
-      vector<vector<int>> ans;
-       while(q.size()!=0){
-        int size = q.size();
-        vector<int> a;
-        for(int i=0;i<size;i++){
-        int node = q.front();
-        a.push_back(node);
-        q.pop();
-        for( auto a2: adj[node]){
-        if(!vis[a2]){
-            vis[a2] = true;
-            q.push(a2);
+        unordered_map<int, vector<int>> adj;
+        buildgraph(root, adj); 
+        queue<int> q;
+        unordered_map<int, bool> visited;
+        visited[target->val] = true;
+        q.push(target->val);
+        int level = 0;
+        vector<int> ans;
+        
+        while (!q.empty()) {
+            int a = q.size();
+            if (level == k) {
+                while (!q.empty()) {
+                    ans.push_back(q.front());
+                    q.pop();
+                }
+                break; 
+            }
+            for (int i = 0; i < a; i++) {
+                int b = q.front();
+                q.pop();
+                for (int n : adj[b]) {
+                    if (!visited[n]) {
+                        q.push(n);
+                        visited[n] = true;
+                    }
+                }
+            }
+            level++;
         }
-        }
-        }
-      ans.push_back(a);
-       }
-       if(ans.size()>=k){
-        return ans[k];
-       }
-      return {};
+        return ans;
     }
 };
