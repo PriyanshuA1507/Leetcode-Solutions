af0c701aa211903f9a8b96c91379cc79bb276812
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2024-12-20 12:23:28 +0530

    Time: 3 ms (44.20%) | Memory: 15.5 MB (38.44%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
@@ -1,39 +1,38 @@
 class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-         map<int, map<int, vector<int>>> nodes;
-        queue<pair<TreeNode*, pair<int, int>>> q;
-        vector<vector<int>> ans;
+        map<int, map<int, vector<int>>> nodes; // To store nodes by horizontal and level order
+        queue<pair<TreeNode*, pair<int, int>>> q; // Queue for BFS traversal
         
-        if (root == nullptr)
-            return ans;
-        
-        q.push(make_pair(root, make_pair(0, 0)));
+        q.push(make_pair(root, make_pair(0, 0))); // Push root with horizontal distance and level
         
         while (!q.empty()) {
-            pair<TreeNode*, pair<int, int>> temp = q.front();
-            q.pop();
-            TreeNode* frontNode = temp.first;
-            int hd = temp.second.first;
-            int lvl = temp.second.second;
+            auto temp = q.front(); 
+            q.pop(); // Pop the front element
             
-            nodes[hd][lvl].push_back(frontNode->val);
+            TreeNode* frontNode = temp.first;
+            int hd = temp.second.first; // Horizontal distance
+            int lvl = temp.second.second; // Level
+            
+            nodes[hd][lvl].push_back(frontNode->val); // Insert into map
             
             if (frontNode->left) {
-                q.push(make_pair(frontNode->left, make_pair(hd - 1, lvl + 1)));
+                q.push(make_pair(frontNode->left, make_pair(hd - 1, lvl + 1))); // Push left child
             }
             if (frontNode->right) {
-                q.push(make_pair(frontNode->right, make_pair(hd + 1, lvl + 1)));
+                q.push(make_pair(frontNode->right, make_pair(hd + 1, lvl + 1))); // Push right child
             }
         }
         
-        for (auto& i : nodes) {
-            vector<int> column;
-            for (auto& j : i.second) {
-                 sort(j.second.begin(), j.second.end());
-                column.insert(column.end(), j.second.begin(), j.second.end());
+        vector<vector<int>> ans; // Final answer
+        
+        for (auto& p : nodes) {
+            vector<int> col; // To store values in one vertical line
+            for (auto& q : p.second) {
+                sort(q.second.begin(), q.second.end()); // Sort values at the same level
+                col.insert(col.end(), q.second.begin(), q.second.end()); // Merge into column
             }
-            ans.push_back(column);
+            ans.push_back(col); // Add column to result
         }
         
         return ans;
