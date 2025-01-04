66ee89ce2f3adada912d8a25f1a14c5610714566
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2025-01-04 07:24:37 +0530

    Time: 167 ms (14.86%) | Memory: 198.6 MB (7.83%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -1,19 +1,26 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        vector<int>ans;
-       list<int>l;
-       int i=0, j=0;
-       while(j < nums.size()){
-          while(l.size() > 0 && l.back() < nums[j])    l.pop_back();
-            l.push_back(nums[j]);
-            if(j-i+1 < k)    j++;
-            else if (j-i+1 == k) {
-                ans.push_back(l.front());
-                if(nums[i] == l.front())    l.pop_front();
-                i++, j++;
-            }
-       }
-    return ans;  
+        vector<int> ans;
+        int i=0, j=0;
+       list<int> l;
+        while(j<nums.size()){
+       while(l.size()!=0 && l.back()<nums[j])
+       l.pop_back();
+        
+        l.push_back(nums[j]);
+        if(j-i+1<k){
+            j++;
+        }
+        else if(j-i+1==k){
+         ans.push_back(l.front());
+         if(nums[i]==l.front()){
+        l.pop_front();
+         }
+         i++;
+         j++;
+        }
+        }
+        return ans;
     }
 };
