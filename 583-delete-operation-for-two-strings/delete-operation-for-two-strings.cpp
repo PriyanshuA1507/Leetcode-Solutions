0f015b80b0aaae290be9dc43f67a7f5a3bb75a51
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2025-01-10 07:09:28 +0530

    Time: 974 ms (5.01%) | Memory: 736.9 MB (5.01%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
@@ -1,25 +1,29 @@
 class Solution {
 public:
-    int solve(string word1, string word2,int i,int j,vector<vector<int>>& dp){
-        if(i>=word1.size() ){
-            return word2.size()-j;
+    int solve(string word1, string word2,int index1,int index2,vector<vector<int>>& dp){
+        if(index1>=word1.size() && index2<word2.size()){
+            return word2.size()-index2;
         }
-        if(j>=word2.size() ){
-            return word1.size()-i;
+        if(index2>=word2.size() && index1<word1.size()){
+            return word1.size()-index1;
         }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
+        if(index1>=word1.size() && index2>=word2.size()){
+            return 0;
         }
-        if(word1[i]==word2[j]){
-            return dp[i][j]=solve(word1,word2,i+1,j+1,dp);
+        if(dp[index1][index2]!=-1){
+            return dp[index1][index2];
+                    }
+        if(word1[index1]==word2[index2]){
+        return solve(word1,word2,index1+1,index2+1,dp);
         }
-        else{
-            return dp[i][j]=min(1+solve(word1,word2,i+1,j,dp), 1 + solve(word1,word2,i,j+1,dp));
-        }
+        int o1 = 1+solve(word1,word2,index1+1,index2,dp);
+        int o2 = 1+solve(word1,word2,index1,index2+1,dp);
+
+        return dp[index1][index2]=min(o1,o2);
     }
     int minDistance(string word1, string word2) {
-        int n = max(word1.size(),word2.size());
-        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
-        return solve(word1,word2,0,0,dp);
+        int n1 = word1.size(), n2 = word2.size();
+        vector<vector<int>> dp(n1+1,vector<int>(n2+1,-1));
+      return solve(word1,word2,0,0,dp);  
     }
 };
