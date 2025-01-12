c5099da43c13eedb53514bd6a0f65f5b7ed83c69
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2025-01-12 18:05:40 +0530

    Time: 5 ms (74.80%) | Memory: 27.4 MB (37.31%) - LeetSync

--- 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.cpp
+++ 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
-    int solve(vector<vector<int>>& pairs,int index,int a,vector<int>& dp){
-        if(index>=pairs.size()){
-            return 0;
-        }
-        if(dp[index]!=-1){
-            return dp[index];
-        }
-        int take = 0;
-        if(pairs[index][0]>a){
-        take = 1 + solve(pairs,index+1,pairs[index][1],dp);
-        }
-        int nottake = solve(pairs,index+1,a,dp);
-        return dp[index]=max(take,nottake);
+    int solve(vector<vector<int>>& pairs,int index,int a, vector<int>& dp){
+    if(index>=pairs.size()){
+        return 0;
+    }
+    int take = 0;
+    if(dp[index]!=-1){
+        return dp[index];
+    }
+    if(pairs[index][0]>a){
+     take = 1 + solve(pairs,index+1,pairs[index][1],dp);
+    }
+    int nottake = solve(pairs,index+1,a,dp);
+
+    return dp[index]=max(take,nottake);
     }
     int findLongestChain(vector<vector<int>>& pairs) {
-        vector<int> dp(pairs.size()+1,-1);
-
-       sort(pairs.begin(), pairs.end(), [](const vector<int>& a, const vector<int>& b) {
+    sort(pairs.begin(), pairs.end(), [](const vector<int>& a, const vector<int>& b) {
             return a[1] < b[1];
         });
-    return solve(pairs,0,INT_MIN,dp);    
+
+     int n = pairs.size();
+     vector<int> dp(n+1,-1);
+     return solve(pairs,0,INT_MIN,dp);   
     }
 };
