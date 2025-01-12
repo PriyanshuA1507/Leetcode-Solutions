e6afbe5d23fad79d29b90495f200fa5d2c0ce879
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2025-01-12 12:57:28 +0530

    Time: 15 ms (24.32%) | Memory: 19.5 MB (27.21%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
-    int solve(vector<int>& piles,int si,int ei,vector<vector<int>>& dp){
-        if(si>ei){
-            return 0;
-        }
-        if(dp[si][ei]!=-1){
-            return dp[si][ei];
-        }
-    int a = piles[si]+min(solve(piles,si+2,ei,dp),solve(piles,si+1,ei-1,dp));
-     int b = piles[ei]+min(solve(piles,si+1,ei-1,dp),solve(piles,si,ei-2,dp));
-     return dp[si][ei]=max(a,b);
-      
+   int solve(vector<int>& piles,int start,int end,vector<vector<int>>& dp){
+    if(start>end){
+        return 0;
     }
+    if(dp[start][end]!=-1){
+        return dp[start][end];
+    }
+    int takestart = piles[start] + min(solve(piles,start+2,end,dp), solve(piles,start+1,end-1,dp));
+    int takeend = piles[end] + min(solve(piles,start+1,end-1,dp), solve(piles,start,end-2,dp));
+    return dp[start][end] = max(takestart,takeend);
+   }
     bool stoneGame(vector<int>& piles) {
+        int n = piles.size();
+        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
+     int p1= solve(piles,0,n-1,dp);  
      int sum = 0;
-     int n = piles.size();
-     vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
-        int ans = solve(piles,0,piles.size()-1,dp);
-    for(int i=0;i<piles.size();i++){
-        sum+=piles[i];
-    }
-    if(ans>sum-ans){
-     return true;   
-    }
-    return false;
+
+     for(auto a:piles){
+        sum+=a;
+     } 
+     if(p1>sum-p1){
+        return true;
+     }
+     return false;
     }
 };
