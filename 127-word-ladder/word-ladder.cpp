306964f694171b8e576c5a08fad49e716f397080
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2025-01-15 08:09:59 +0530

    Time: 540 ms (24.79%) | Memory: 161.9 MB (5.01%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -1,29 +1,40 @@
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-    queue<pair<string,int>> q;
-    q.push({beginWord,0});
-    unordered_set s(wordList.begin() , wordList.end());
-    s.erase(beginWord);
-    while(q.size()!=0){
-    string word = q.front().first;
-    int dist = q.front().second;
-    q.pop();
-    if(word==endWord){
-        return dist+1;
-    }
-    for(int i=0;i<word.size();i++){
-     char originalchar = word[i];
-     for(char ch='a';ch<='z';ch++){
-        word[i] = ch;
-     if(s.find(word)!=s.end()){
-        s.erase(word);
-    q.push({word, dist+1});
+    queue<string> q;
+     unordered_map<string,bool> m;
+     for(auto a:wordList){
+      m[a]= true;
      }
+     if(!m[endWord]){
+        return 0;
      }
-     word[i] = originalchar;
-    }
-    }
-    return 0;
+     unordered_map<string,bool> visit;
+     int ans = 1;
+     q.push(beginWord);
+     visit[beginWord] = true;
+
+     while(q.size()!=0){
+     int a = q.size();
+     for(int i=0;i<a;i++){
+     auto b = q.front();
+     q.pop();
+     if(b==endWord){
+        return ans;
+     }
+     for (int j = 0; j < b.size(); j++) {
+                    string temp = b;
+                    for (char c = 'a'; c <= 'z'; c++) {
+                        temp[j] = c;
+                        if (m[temp] && !visit[temp]) {
+                            visit[temp] = true; 
+                            q.push(temp);
+                        }
+                    }
+                }
+     }
+      ans++;
+     }
+     return 0;  
     }
 };
