ad634a4f91012612c10ba97466336726497b70cf
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2025-01-16 12:55:45 +0530

    Time: 0 ms (100.00%) | Memory: 14.4 MB (9.24%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
@@ -1,28 +1,39 @@
 class Solution {
 public:
-    int solve(vector<int>& days, vector<int>& costs,int index,vector<int>& dp){
+    int solve(vector<int>& days, vector<int>& costs, int index, vector<int>& dp) {
         int n = days.size();
-        if(index>=n){
+        if (index >= n) {
             return 0;
         }
-        if(dp[index]!=-1){
+        if (dp[index] != -1) {
             return dp[index];
         }
-        int cost1 = costs[0] + solve(days,costs,index+1,dp);
-       
-       int i;
-       for(i = index;i<n && days[i]<days[index]+7;i++);
-        int cost2= costs[1] + solve(days,costs,i,dp);
-        
-         for(i = index;i<n && days[i]<days[index]+30;i++);
-        int cost3= costs[2] + solve(days,costs,i,dp);
-        
 
-        return dp[index]=min({cost1,cost2,cost3});
+    
+        int o = costs[0] + solve(days, costs, index + 1, dp);
+
+        int s = INT_MAX;
+        int i = index;
+        while (i < n && days[i] < days[index] + 7) {
+            i++;
+        }
+        s = costs[1] + solve(days, costs, i, dp);
+
+
+        int t = INT_MAX;
+        i = index;
+        while (i < n && days[i] < days[index] + 30) {
+            i++;
+        }
+        t = costs[2] + solve(days, costs, i, dp);
+
+    
+        return dp[index] = min({o, s, t});
     }
+
     int mincostTickets(vector<int>& days, vector<int>& costs) {
         int n = days.size();
-        vector<int> dp(366,-1);
-       return solve(days,costs,0,dp); 
+        vector<int> dp(n + 1, -1); // Correct size of dp array
+        return solve(days, costs, 0, dp);
     }
 };
