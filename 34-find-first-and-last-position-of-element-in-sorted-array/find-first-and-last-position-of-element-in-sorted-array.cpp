db74ce249c5cbd650f9148112b6b97acc0597155
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2025-01-20 08:13:06 +0530

    Time: 0 ms (100.00%) | Memory: 17.5 MB (80.26%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,39 +1,30 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        if (nums.size() == 1 && target == nums[0]) {
+         if (nums.size() == 1 && target == nums[0]) {
             return {0, 0};
         }
-
-        int start = 0;
-        int end = nums.size() - 1;
-
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-
-            if (nums[mid] == target) {
-                int left = mid, right = mid;
-
-                // Find the left boundary of the range
-                while (left > 0 && nums[left - 1] == target) {
-                    left--;
-                }
-
-                // Find the right boundary of the range
-                while (right < nums.size() - 1 && nums[right + 1] == target) {
-                    right++;
-                }
-
-                return {left, right};
-            }
-
-            if (nums[mid] > target) {
-                end = mid - 1;
-            } else {
-                start = mid + 1;
-            }
+        int start=0;
+        int end = nums.size()-1;
+        while(start<=end){
+        int m = (start+end)/2;
+        if(nums[m]==target){
+        int left = m, right = m;
+        while(left>0 && nums[left-1]==target){
+         left--;
         }
-
-        return {-1, -1};
+         while(right<nums.size()-1 && nums[right+1]==target){
+         right++;
+        }
+        return {left,right};
+        }
+        else if(nums[m]>target){
+         end = m-1;
+        }
+        else if(nums[m]<target){
+            start = m+1;
+        }
+        }
+        return {-1,-1};
     }
 };
