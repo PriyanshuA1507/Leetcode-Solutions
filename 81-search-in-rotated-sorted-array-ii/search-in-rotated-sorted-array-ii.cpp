674b5fc47af0f5dba68592e8fbc1a72d36035c02
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2025-01-28 00:47:52 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (89.46%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -1,35 +1,34 @@
 class Solution {
 public:
-    bool search(vector<int>& arr, int target) {
-        int n = arr.size();
-        int low = 0;
-        int high = n - 1;
-
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-
-            if (arr[mid] == target) return true;
-
-            
-            if (arr[low] == arr[mid] && arr[mid] == arr[high]) {
-                low++;
-                high--;
-            } 
-        
-            else if (arr[low] <= arr[mid]) {
-                if (arr[low] <= target && target < arr[mid]) {
-                    high = mid - 1;
-                } else {
-                    low = mid + 1;
-                }
-            } 
-            else {
-                if (arr[mid] <= target && target <= arr[high]) {
-                    low = mid + 1;
-                } else {
-                    high = mid - 1;
-                }
-            }
+    bool search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int start = 0;
+        int end = n-1;
+        while(start<=end){
+        int mid = start + (end-start)/2;
+        if(nums[mid]==target){
+         return true;
+        }
+        if(nums[start]==nums[mid] && nums[mid]==nums[end]){
+        start++;
+        end--;
+        }
+        else if(nums[start]<=nums[mid]){
+         if(nums[start]<=target && target<=nums[mid]){
+         end = mid-1;
+         }
+         else{
+          start = mid+1;
+         }
+        }
+        else{
+        if(nums[mid]<=target && target<=nums[end]){
+         start = mid+1;
+        }
+        else{
+        end = mid - 1;
+        }
+        }
         }
         return false;
     }
