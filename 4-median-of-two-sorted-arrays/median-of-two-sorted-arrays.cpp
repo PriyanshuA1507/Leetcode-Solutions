7f0bd94e57adf22ea47a3ef596be1dc9198d7682
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2025-01-28 12:32:41 +0530

    Time: 0 ms (100.00%) | Memory: 95 MB (81.17%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        vector<double> b;
-        double ans = 0;
-        priority_queue<int, vector<int>, greater<int>> pq;
-        for(int a:nums1){
-            pq.push(a);
-        } 
-        for(int a:nums2){
-            pq.push(a);
-        } 
-        while(pq.size()!=0){
-            int a = pq.top();
-            pq.pop();
-            b.push_back(a);
+    double findMedianSortedArrays(vector<int>& a, vector<int>& b) {
+        int n1 = a.size() , n2 = b.size();
+        if(n1>n2) return findMedianSortedArrays(b,a);
+        int n = n1+n2;
+        int left = (n1+n2+1)/2;
+        int start = 0, end = n1;
+        while(start<=end){
+        int mid1 = (start+end)>>1;
+        int mid2 = left - mid1;
+        int l1 = INT_MIN, l2 = INT_MIN, r1 = INT_MAX, r2 = INT_MAX;
+        if(mid1<n1) r1 = a[mid1];
+        if(mid2<n2) r2 = b[mid2];
+        if(mid1-1>=0) l1 = a[mid1-1];
+        if(mid2-1>=0) l2 = b[mid2-1];
+        if(l1<=r2 && l2<=r1){
+        if(n%2==1) return max(l1,l2);
+        else return ((double)(max(l1,l2)+min(r1,r2)))/2.0;
         }
-          int mid = b.size()/2;
-        if(b.size()%2==0){
-        ans = (b[mid-1]+b[mid])/2;
+        else if(l1>r2) end = mid1-1;
+        else start = mid1+1;
         }
-        else{
-        ans = (b[mid]);
-        }
-        return ans;
+        return 0;
     }
 };
