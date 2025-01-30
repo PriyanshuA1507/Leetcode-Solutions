ebbee3bf4efb052a3f0c63523985902a6a29d417
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2025-01-31 01:25:54 +0530

    Time: 6 ms (68.87%) | Memory: 71.4 MB (93.22%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
@@ -1,39 +1,27 @@
 class Solution {
-    bool check(vector<int> &a, int k) {
-        int ans = 0;
-        unordered_map<int, int> m;
-        for (int i = 0; i < a.size(); i++) {
-            m[a[i]]++;
-        }
-        for (int i = 0; i < a.size(); i++) {
-            if (m[a[i]] > 0 && a[i] % 2 != 0) {
-                ans++;
-                m[a[i]] = 0;
-            }
-        }
-        return ans >= k;
+public:
+    int solve(vector<int>& nums, int k){
+    int start = 0;
+    int end = 0;
+    int ans = 0;
+    int oc=0;
+    while(end<nums.size()){ 
+     if(nums[end]%2==1){
+     oc++;
     }
 
-public:
-    int numberOfSubarrays(vector<int> &nums, int k) {
-        int count = 0;
-        int oddCount = 0;
-        unordered_map<int, int> prefixCount;
-
-        prefixCount[0] = 1;
-
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] % 2 == 1) {
-                oddCount++;
-            }
-
-            if (prefixCount.find(oddCount - k) != prefixCount.end()) {
-                count += prefixCount[oddCount - k];
-            }
-
-            prefixCount[oddCount]++;
-        }
-
-        return count;
+    while(oc>k){
+    if(nums[start]%2==1){
+      oc--;
+    }
+    start++;
+    }
+    ans+=(end-start+1);
+    end++;
+    }
+    return ans;
+    }
+    int numberOfSubarrays(vector<int>& nums, int k) {
+       return solve(nums,k) - solve(nums,k-1); 
     }
 };
