ec5c07d1a437eabd04595f36e3dd1965a826b596
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2025-02-02 15:17:02 +0530

    Time: 6 ms (50.32%) | Memory: 23.6 MB (91.97%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,30 +1,21 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        if(intervals.size()==1){
-            return intervals;
-        }
-     sort(intervals.begin(), intervals.end(), [](const vector<int>& a, const vector<int>& b) {
-            return a[0] < b[0];
-        });
-        vector<vector<int>> ans;
-        int s = intervals[0][0];
-        int e = intervals[0][1];
-        ans.push_back({s,e});
-        for(int i=1;i<intervals.size();i++){
-            int n = ans.size();
-            if(ans[n-1][1]>=intervals[i][0]){
-                e = max(intervals[i][1],e);
-                s = min(s,intervals[i][0]);
-                ans[n-1][0] = s;
-                ans[n-1][1] = e;
-            }
-            else{
-            ans.push_back({intervals[i][0],intervals[i][1]});
-            s = intervals[i][0];
-            e = intervals[i][1];
+         vector<vector<int>> ans;
+    sort(intervals.begin(), intervals.end(), [](const vector<int>& a, const vector<int>& b) {
+        return a[0] < b[0]; 
+    });
+   ans.push_back(intervals[0]);
+        
+        for (int i = 1; i < intervals.size(); i++) {
+            vector<int>& last = ans.back();
+            if (intervals[i][0] <= last[1]) { 
+                last[1] = max(last[1], intervals[i][1]); 
+            } else {
+                ans.push_back(intervals[i]); 
             }
         }
+        
         return ans;
-        }       
+    }
 };
