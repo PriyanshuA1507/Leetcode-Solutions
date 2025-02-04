05bf1a04058b0fd988f93517e684d908280fc6fd
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2025-02-05 00:07:09 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (50.95%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.cpp
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -1,40 +1,42 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        int m = matrix.size(), n = matrix[0].size();
         vector<int> ans;
-        int fr = 0, fc = 0, lr = m - 1, lc = n - 1;
-        
-        while (fr <= lr && fc <= lc) {
-            // Traverse from left to right along the top row
-            for (int i = fc; i <= lc; i++) {
-                ans.push_back(matrix[fr][i]);
-            }
-            fr++; // Move the top boundary down
-            
-            // Traverse from top to bottom along the last column
-            for (int i = fr; i <= lr; i++) {
-                ans.push_back(matrix[i][lc]);
-            }
-            lc--; // Move the right boundary left
-            
-            // Check if there is a bottom row to traverse
-            if (fr <= lr) {
-                for (int i = lc; i >= fc; i--) {
-                    ans.push_back(matrix[lr][i]);
-                }
-                lr--; // Move the bottom boundary up
-            }
-            
-            // Check if there is a left column to traverse
-            if (fc <= lc) {
-                for (int i = lr; i >= fr; i--) {
-                    ans.push_back(matrix[i][fc]);
-                }
-                fc++; // Move the left boundary right
-            }
-        }
-        
+    int n = matrix.size();
+    int m = matrix[0].size();
+      int firstrow = 0;
+      int lastcol = m-1;
+      int lastrow = n-1;
+      int firstcol = 0;
+      while(ans.size()<m*n){
+    //firstrow
+    
+      for(int i=firstcol;i<=lastcol && ans.size()<m*n;i++){
+        ans.push_back(matrix[firstrow][i]);
+      }
+      firstrow++;
+    
+    //lastcol
+    
+      for(int i=firstrow;i<=lastrow && ans.size()<m*n;i++){
+        ans.push_back(matrix[i][lastcol]);
+      }
+      lastcol--;
+      
+    //lastrow
+      for(int i=lastcol;i>=firstcol  && ans.size()<m*n;i--){
+        ans.push_back(matrix[lastrow][i]);
+      }
+    lastrow--;
+      
+    //firstcol
+
+      for(int i=lastrow;i>=firstrow  && ans.size()<m*n;i--){
+        ans.push_back(matrix[i][firstcol]);
+      }
+    firstcol++;
+      
+      }
         return ans;
     }
 };
