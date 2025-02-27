21da62ff27ebba83eb9e4b673e9f602d8ef4d4c5
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2025-02-28 00:48:04 +0530

    Time: 431 ms (11.96%) | Memory: 152.3 MB (5.13%) - LeetSync

--- 905-length-of-longest-fibonacci-subsequence/length-of-longest-fibonacci-subsequence.cpp
+++ 905-length-of-longest-fibonacci-subsequence/length-of-longest-fibonacci-subsequence.cpp
@@ -1,40 +1,37 @@
-#include <vector>
-#include <unordered_map>
-
-using namespace std;
-
 class Solution {
 public:
-    int solve(vector<int>& arr, int index1, int index2, vector<vector<int>>& dp, unordered_map<int, int>& indexMap) {
-        if (indexMap.find(arr[index1] + arr[index2]) == indexMap.end()) {
-            return 0;
+     int solve(vector<int>& arr,int index1,int index2,vector<vector<int>>& dp,unordered_map<int, int>& mp){
+        if (dp[index1][index2] != -1) {
+            return dp[index1][index2];
         }
 
-        int index3 = indexMap[arr[index1] + arr[index2]];
-        if (dp[index2][index3] != -1) {
-            return dp[index2][index3];
-        }
+        int take = 0;
+        int nextVal = arr[index1] + arr[index2];
 
-        return dp[index2][index3] = 1 + solve(arr, index2, index3, dp, indexMap);
-    }
-
-    int lenLongestFibSubseq(vector<int>& arr) {
-        int n = arr.size();
-        unordered_map<int, int> indexMap;
-        vector<vector<int>> dp(n, vector<int>(n, -1));
-        int ans = 0;
-
-        for (int i = 0; i < n; i++) {
-            indexMap[arr[i]] = i;
-        }
-
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                int length = 2 + solve(arr, i, j, dp, indexMap);
-                ans = max(ans, length);
+        if (mp.count(nextVal)) { 
+            int nextIndex = mp[nextVal];
+            if (nextIndex > index2) { 
+                take = 1 + solve(arr, index2, nextIndex, dp, mp);
             }
         }
 
-        return ans > 2 ? ans : 0;
+        return dp[index1][index2] = take;
+     }
+    int lenLongestFibSubseq(vector<int>& arr) {
+        int n = arr.size();
+       vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
+        int ans = 0;
+         unordered_map<int, int> mp;
+        for (int i = 0; i < n; i++) {
+            mp[arr[i]] = i;  
+        }
+        for(int i=0;i<arr.size();i++){
+        for(int j=i+1;j<arr.size();j++){
+        if(solve(arr,i,j,dp,mp)>0){
+         ans = max(ans, 2 + solve(arr,i,j,dp,mp));
+        }
+        }
+        }
+        return ans;
     }
 };
