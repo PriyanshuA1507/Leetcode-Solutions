69febb6cd07bfc46b7f3d1d785992695a285b083
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2025-03-16 19:06:22 +0530

    Time: 23 ms (19.50%) | Memory: 69.3 MB (33.09%) - LeetSync

--- 2591-frog-jump-ii/frog-jump-ii.cpp
+++ 2591-frog-jump-ii/frog-jump-ii.cpp
@@ -1,22 +1,35 @@
 class Solution {
 public:
+    bool check(vector<int>& stones,int mid){
+    for(int i=2;i<stones.size();i+=2){
+    if(stones[i]-stones[i-2]>mid){
+     return false;
+    }
+    }
+    for(int i=3;i<stones.size();i+=2){
+    if(stones[i]-stones[i-2]>mid){
+     return false;
+    }
+    }
+    return true;
+    }
     int maxJump(vector<int>& stones) {
-        int n = stones.size();
-        int maxJump = 0;
-
-        // Check jumps by moving forward skipping one stone at a time
-        for (int i = 2; i < n; i += 2) {
-            maxJump = max(maxJump, stones[i] - stones[i - 2]);
-        }
-
-        // Check jumps by moving backward skipping one stone at a time
-        for (int i = 3; i < n; i += 2) {
-            maxJump = max(maxJump, stones[i] - stones[i - 2]);
-        }
-
-        // Edge case: Compare the jump from the last to second last stone
-        maxJump = max(maxJump, stones[n - 1] - stones[n - 2]);
-
-        return maxJump;
+    if(stones.size()==2){
+        return stones[1]-stones[0];
+    }
+     int start = 1;
+     int ans = 0;
+     int end = *max_element(stones.begin(),stones.end());
+     while(start<=end){
+    int mid = start + (end-start)/2;
+    if(check(stones,mid)){
+        ans = mid;
+        end = mid-1;
+    }
+    else{
+    start = mid+1;
+    }
+    }
+    return ans;   
     }
 };
