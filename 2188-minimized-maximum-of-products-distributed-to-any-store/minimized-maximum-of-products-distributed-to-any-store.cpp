aba8dd3a23accf6b57fc39d68e7eb92aec91abcb
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2025-03-16 17:30:13 +0530

    Time: 35 ms (52.54%) | Memory: 88.5 MB (17.89%) - LeetSync

---
+++ 2188-minimized-maximum-of-products-distributed-to-any-store/minimized-maximum-of-products-distributed-to-any-store.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool check(int n, vector<int>& q,int mid){
+        int ans = 0;
+        for(auto a:q){
+         ans+=(a + mid - 1) / mid;
+        }
+        return ans<=n;
+        }
+    int minimizedMaximum(int n, vector<int>& quantities) {
+        int start = 1;
+        int end = *max_element(quantities.begin(), quantities.end());
+        int ans = end;
+        while(start<=end){
+        int mid = start + (end-start)/2;
+        if(check(n,quantities,mid)){
+        ans = mid;
+        end = mid-1;
+        }
+        else{
+            start = mid+1;
+        }
+        }
+        return ans;
+    }
+};
