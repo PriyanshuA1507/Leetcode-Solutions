443611b676918795bed7db1b515c21cedb11138c
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2025-03-19 21:50:06 +0530

    Time: 80 ms (8.56%) | Memory: 72.9 MB (5.64%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -1,34 +1,33 @@
 class Solution {
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        vector<int> ans;
-        int n = graph.size();
-        queue<int> q;
-        vector<int> indegree(n,0);
-        unordered_map<int,vector<int>> adj;
-        for(int i=0;i<graph.size();i++){
-         for(auto a:graph[i]){
-         adj[a].push_back(i);
-         indegree[i]++;
-         }
+     unordered_map<int,vector<int>> adj;
+     vector<int> indegree(graph.size(),0);
+     for(int i=0;i<graph.size();i++){
+     for(auto a:graph[i]){
+     adj[a].push_back(i);
+     indegree[i]++;
+     }
+     }
+     queue<int> q;
+     for(int i=0;i<graph.size();i++){
+     if(indegree[i]==0){
+     q.push(i);
+     }
+     }
+    vector<int> ans;
+    while(q.size()!=0){
+    int a = q.front();
+    q.pop();
+    ans.push_back(a);
+    for(auto b:adj[a]){
+        indegree[b]--;
+        if(indegree[b]==0){
+        q.push(b);
         }
-        for(int i=0;i<n;i++){
-            if(indegree[i]==0){
-                q.push(i);
-            }
-        }
-        while(q.size()!=0){
-        auto a = q.front();
-        ans.push_back(a);
-        q.pop();
-        for(auto v:adj[a]){
-            indegree[v]--;
-            if(indegree[v]==0){
-            q.push(v);
-            }
-        }
-        }
-        sort(ans.begin(),ans.end());
-        return ans;
+    }
+    }
+    sort(ans.begin(),ans.end());
+   return ans;
     }
 };
