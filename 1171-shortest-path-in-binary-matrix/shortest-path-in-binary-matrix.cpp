13090994371fcaffb7c342e43b8219708e92d795
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2025-03-19 23:04:57 +0530

    Time: 313 ms (5.06%) | Memory: 84.7 MB (5.01%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -1,38 +1,31 @@
 class Solution {
 public:
-    using p = pair<int,int>;
-    vector<vector<int>> dirs = {{-1,0},{-1,1},{0,1},{1,1},{1,0},{1,-1},{0,-1},{-1,-1}};
-    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-    int n = grid.size();
-    if(grid[0][0] == 1 || grid[n-1][n-1] == 1) {
-            return -1;
-        }
-    vector<vector<bool>> visited(n,vector<bool>(n,false));
-    queue<p> q;
-    q.push({0,0});
-    int pathlength=1;
-    visited[0][0] = true;
+  using p = pair<long long,pair<long long,long long>>;
+   vector<vector<long long>> dir = {{-1,0},{-1,1},{0,1},{1,1},{1,0},{1,-1},{0,-1},{-1,-1}};
+    long long shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+    long long n = grid.size();
+    priority_queue<p,vector<p>,greater<p>>q;
+    vector<vector<long long>> result(n,vector<long long>(n,INT_MAX));
+    result[0][0] = 0;
+    q.push({0,{0,0}});
     while(q.size()!=0){
-    int size = q.size();
-    for(int i=0;i<size;i++){
-    auto a = q.front();
-    int j = a.first;
-    int k = a.second;
+    auto a = q.top();
     q.pop();
-    if(j==n-1 && k==n-1){
-        return pathlength;
+    long long fd = a.first;
+    long long fi = a.second.first;
+    long long fj = a.second.second;
+    for(auto d:dir){
+    long long ni = fi + d[0];
+    long long nj = fj + d[1];
+    if(ni>=0 && nj>=0 && ni<n && nj<n && grid[ni][nj]==0 && result[ni][nj]>fd+1){
+     result[ni][nj] = fd+1;
+     q.push({fd+1,{ni,nj}});
     }
-    for(auto d:dirs){
-    int r = j + d[0];
-    int c = k + d[1];
-    if(r>=0 && c>=0 && r<n && c<n && !visited[r][c] && grid[r][c]==0){
-        visited[r][c] = true;
-        q.push({r,c});
     }
     }
+    if(grid[0][0]==1 || grid[n-1][n-1]==1 || result[n-1][n-1]==INT_MAX){
+        return -1;
     }
-    pathlength++;
-    }
-     return -1;
+    return result[n-1][n-1]+1;
     }
 };
