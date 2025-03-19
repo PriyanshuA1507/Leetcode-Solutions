6c54e319b57d5aa84fb20ae7cb8f99a05271fd2c
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2025-03-19 18:29:35 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (23.88%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.cpp
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -1,44 +1,41 @@
 class Solution {
 public:
-   using p = pair<int,int>;
-   vector<vector<int>> dirs = {{-1,0},{0,1},{1,0},{0,-1}};
-    void solve(vector<vector<char>>& board) {
-        int n = board.size();
-        int m = board[0].size();
-        queue<p>q;
-        for(int i=0;i<board.size();i++){
-        for(int j=0;j<board[0].size();j++){
-         if(board[i][j]=='O' && (i==0 || i==n-1 || j==0 || j==m-1)){
-            q.push({i,j});
-            board[i][j]='c';
-         }
-        }
-        }
-        while(q.size()!=0){
-        int s = q.size();
-        for(int i=0;i<s;i++){
-        auto a = q.front();
-        q.pop();
-        for(auto d: dirs){
-        int r = a.first + d[0];
-        int c = a.second + d[1];
+ vector<vector<int>> dir = {{-1,0},{0,1},{1,0},{0,-1}};
+  using p = pair<int,int>;
+    void solve(vector<vector<char>>& grid) {
+           int m = grid.size(), n = grid[0].size();
+    queue<p> q;
+    for(int i=0;i<grid.size();i++){
+    for(int j=0;j<grid[0].size();j++){
+    if((i == 0 || i == m - 1 || j == 0 || j == n - 1) && grid[i][j] == 'O'){
+                    q.push({i, j});
+                    grid[i][j] = 'J';
+      }
+    }
+    }
+    
+   while(!q.empty()){
+            auto b = q.front();
+            q.pop();
+            int x = b.first, y = b.second; 
+            
+            for(auto d : dir){
+                int ni = x + d[0];
+                int nj = y + d[1];
 
-        if(r>=0 && c>=0 && r<n && c<m && board[r][c]=='O'){
-            board[r][c] = 'c';
-            q.push({r,c});
-        }
-        }
-        }
-        }
-        for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-        if(board[i][j]=='O' ){
-            board[i][j]='X';
-        }
-        if (board[i][j] == 'c') {
-                    board[i][j] = 'O'; 
+                if(ni >= 0 && nj >= 0 && ni < m && nj < n && grid[ni][nj] == 'O'){
+                    q.push({ni, nj});
+                    grid[ni][nj] = 'J'; 
                 }
+            }
         }
+
+    for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(grid[i][j] == 'O') grid[i][j] = 'X';
+                if(grid[i][j] == 'J') grid[i][j] = 'O';
+            }
         }
+           
     }
 };
