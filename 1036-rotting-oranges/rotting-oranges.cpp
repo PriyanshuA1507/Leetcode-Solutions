6acd05800e07061ea58f7fc7890afabd77061689
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2025-03-19 14:47:27 +0530

    Time: 4 ms (18.31%) | Memory: 18.1 MB (7.19%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,48 +1,47 @@
 class Solution {
 public:
-  using p = pair<int,int>;
-    vector<vector<int>> dir = {{-1,0},{0,1},{1,0},{0,-1}};
+ using p = pair<int,int>;
+ vector<vector<int>> dir = {{-1,0},{0,1},{1,0},{0,-1}};
     int orangesRotting(vector<vector<int>>& grid) {
-    
-    int m = grid.size();
-    int n = grid[0].size();
+        int fo = 0;
+        int ans =0;
+        queue<p> q;
+        for(int i=0;i<grid.size();i++){
+        for(int j=0;j<grid[0].size();j++){
+        if(grid[i][j]==1){
+            fo++;
+        }
+        else{
+            if(grid[i][j]==2){
+            q.push({i,j});
+            }
+        }
+        }
+        }
 
-    queue<p>q;
-    int fo=0;
-    for(int i=0;i<m;i++){
-    for(int j=0;j<n;j++){
-     if(grid[i][j]==2){
-     q.push({i,j});
-     }
-     if(grid[i][j]==1){
-     fo++;
-     }
-    }
-    }
-    if(fo==0){
-        return 0;
-    }
-    int time = 0;
-    while(q.size()!=0){
-     int s = q.size();
-     time++;
-     for(int i=0;i<s;i++){
-     auto p = q.front();
-     q.pop();
-     for(auto d:dir){
-     int i = p.first + d[0];
-     int j = p.second + d[1];
-     if(i>=0 && j>=0 && i<m && j<n && grid[i][j]==1){
-      grid[i][j]=2;
-      fo--;
-      q.push({i,j});
-     }
-     }
-     }
-    }
-    if(fo==0){
-        return time-1;
-    }
+     if(fo==0){
+            return 0;
+        }
+        while(q.size()!=0){
+        int c = q.size();
+        for(int i=0;i<c;i++){
+        auto a = q.front();
+        q.pop();
+        for(auto b:dir){
+        int i = a.first + b[0];
+        int j = a.second + b[1];
+        if(i>=0 && i<grid.size() && j>=0 && j<grid[0].size() && grid[i][j]==1){
+         q.push({i,j});
+         grid[i][j]=2;
+         fo--;
+        }
+        }
+        }
+        ans++;
+        }
+   if(fo!=0){
     return -1;
+   }
+  return ans-1;
     }
 };
