8ad77279d52114c150e87de10ac055540a1bc4b0
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2025-03-19 18:54:55 +0530

    Time: 0 ms (100.00%) | Memory: 17.3 MB (56.71%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -1,32 +1,30 @@
 class Solution {
 public:
-    bool checkbypatrite(vector<vector<int>>& graph,int i,int cc,vector<int>& color){
-        color[i] = cc;
-        for(auto v:graph[i]){
-        if(color[v]==color[i]){
-            return false;
-        }
-        if(color[v]==-1){
-        int colorofv = 1 - cc;
-        if(checkbypatrite(graph,v,colorofv,color)==false){
-            return false;
-        }
-        }
-        }
-        return true;
-
-    }
+   bool check(vector<vector<int>>& adj,int curr,vector<int>& color,int cc){
+  color[curr] = cc;
+   for(int & v: adj[curr]){
+if(color[v]==color[curr]){
+    return false;
+}
+if(color[v]==-1){
+int ccv = 1 - cc;
+if(check(adj,v,color,ccv)==false){
+    return false;
+}
+}
+}
+return true; 
+}
     bool isBipartite(vector<vector<int>>& graph) {
-        int n = graph.size();
-        vector<int> color(n,-1);
-        for(int i=0;i<n;i++){
-        if(color[i]==-1){
-        if(checkbypatrite(graph,i,1,color)==false){
-            return false;
-        }
-        }
-        }
-        return true;
-
+    int V = graph.size();
+    vector<int> color(V,-1);
+    for(int i=0;i<V;i++){
+    if(color[i]==-1){
+    if(check(graph,i,color,1)==false){
+        return false;
+    }
+    }
+    }
+    return true;
     }
 };
