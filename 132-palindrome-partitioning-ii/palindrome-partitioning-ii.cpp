863eda5f315b703024c2660830199174be1b9b5c
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2025-03-20 21:15:32 +0530

    Time: 1781 ms (8.81%) | Memory: 9.4 MB (69.29%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
@@ -1,38 +1,34 @@
 class Solution {
 public:
-    bool check(string& s,int start, int end) {
-        bool a = true;
-        int start1 = start, end1 = end;
-        while (start1 < end1) {
-            if (s[start1] != s[end1]) {
-                a = false;
-                break;
-            }
-            start1++;
-            end1--;
-        }
-        return a; 
+   bool ispalin(int i,int j,string& s){
+    while(i<j){
+    if(s[i]!=s[j]){
+        return false;
     }
-
-    int solve(string& s, int start, int end,vector<vector<int>>& dp) {
-        if (start >= end ||  check(s, start, end)) {
-            return 0;
-        }
-        if(dp[start][end]!=-1){
-            return dp[start][end];
-        }
-        int ans = 1e9;
-        for (int k = start ; k <end; k++) {
-            if (check(s,start,k)) {
-                ans = min(ans, 1 + solve(s,k+1,end,dp));
-            }
-        }
-        return dp[start][end]=ans;
+    i++;
+    j--;
     }
-
+    return true;
+   }
+   int mp(string& s,int start,int n, vector<int>& dp){
+    if(start>=n){
+        return 0;
+    }
+    if(dp[start]!=-1){
+        return dp[start];
+    }
+    int mc = 1e8;
+    for(int i=start;i<n;i++){
+     if(ispalin(start,i,s)){
+     int cost = 1 + mp(s,i+1,n,dp);
+     mc = min(cost,mc);
+     }
+    }
+    return dp[start]=mc;
+   }
     int minCut(string s) {
         int n = s.size();
-        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
-        return solve(s, 0, n - 1,dp);
+        vector<int> dp(n+1,-1);
+    return mp(s,0,s.size(),dp)-1;
     }
 };
