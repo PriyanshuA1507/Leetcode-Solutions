fa1563d1facbd1a1e11399798bf9a25f8a6f0bbb
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2025-03-26 08:35:25 +0530

    Time: 512 ms (14.63%) | Memory: 9.3 MB (63.09%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool check(string &s, int start, int end) {
+    bool check(string &s, int start, int end,vector<int>& dp) {
         while (start <= end) {
             if (s[start] != s[end]) {
                 return false;
@@ -11,21 +11,26 @@ public:
         return true;
     }
 
-    int solve(string &s, int index) {
+    int solve(string &s, int index,vector<int>& dp) {
         if (index >= s.size()) {
             return 0;
+        }
+        if(dp[index]!=-1){
+            return dp[index];
         }
 
         int count = 0;
         for (int i = index; i < s.size(); i++) {
-            if (check(s, index, i)) {
-                count++; // Count this palindrome
+            if (check(s, index, i,dp)) {
+                count++; 
             }
         }
-        return count + solve(s, index + 1); // Move to the next starting index
+        return dp[index]=count + solve(s, index + 1,dp); 
     }
 
     int countSubstrings(string s) {
-        return solve(s, 0);
+        int n = s.size();
+        vector<int> dp(n+1,-1);
+        return solve(s, 0,dp);
     }
 };
