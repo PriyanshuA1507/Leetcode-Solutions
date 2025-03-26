f28a781bc694c102409dc4f5def8c0fa37530c7e
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2025-03-26 08:13:23 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (25.56%) - LeetSync

--- 413-arithmetic-slices/arithmetic-slices.cpp
+++ 413-arithmetic-slices/arithmetic-slices.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
-   int solve(vector<int>& nums,int index,int d,int pi,int n){
-    if(index>=n || nums[index]-nums[pi]!=d){
+    int solve(int ci,int pi,int d,vector<int>& nums){
+    if(ci>=nums.size()){
         return 0;
     }
-    return  1 + solve(nums,index+1,d,index,n);
-
-   }
+    int take = 0;
+    if(nums[ci]-nums[pi]==d){
+     take = 1 + solve(ci+1,ci,d,nums);
+    }
+    return take ;
+    }
     int numberOfArithmeticSlices(vector<int>& nums) {
-        int n = nums.size();
-        if(n<3){
-            return 0;
-        }
-
-        int ans = 0;
-        for(int i=0;i<n-2;i++){
-        int d = nums[i+1] - nums[i];
-        ans+=solve(nums,i+2,d,i+1,n);
-        }
-        return ans;
+      if(nums.size()<3){
+        return 0;
+      }  
+      int ans = 0;
+      for(int i=0;i<nums.size();i++){
+       for(int j=i+1;j<nums.size();j++){
+       ans+=(solve(j+1,j,nums[j]-nums[i],nums));
+       break;
+       }
+      }
+      return ans;
     }
 };
