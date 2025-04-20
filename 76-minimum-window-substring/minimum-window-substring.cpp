9e18f3328920b75802d848a405e53283843b854f
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2025-04-21 01:50:30 +0530

    Time: 7 ms (67.73%) | Memory: 11.4 MB (64.61%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,43 +1,35 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-   unordered_map<char, int> m;
-        for (char c : t) {
-            m[c]++;
-        }
-        
-        int count = m.size(); 
-        int start = 0, end = 0;
-        int minLen = INT_MAX;
-        int minStart = 0;
-        
-        while (end < s.size()) {
-            if (m.find(s[end]) != m.end()) {
-                m[s[end]]--;
-                if (m[s[end]] == 0) {
-                    count--;
-                }
-            }
-            
-            while (count == 0) { 
-                if (end - start + 1 < minLen) {
-                    minLen = end - start + 1;
-                    minStart = start;
-                }
-                
-                if (m.find(s[start]) != m.end()) {
-                    m[s[start]]++;
-                    if (m[s[start]] > 0) {
-                        count++;
-                    }
-                }
-                
-                start++;
-            }
-            
-            end++;
-        }
-        
-        return minLen == INT_MAX ? "" : s.substr(minStart, minLen);
+        int n = s.size();
+       unordered_map<char,int> m;
+       for(auto a:t){
+        m[a]++;
+       } 
+       int minstart = 0;
+       int minwindow = INT_MAX;
+       int requiredcount = t.size();
+       int left = 0, right = 0;
+       while(right<n){
+      char ch_j = s[right];
+      if(m[s[right]]>0){
+        requiredcount--;
+      }
+      m[ch_j]--;
+      while(requiredcount == 0){
+    if(minwindow>right-left+1){
+    minwindow = right-left + 1;
+    minstart = left;
+    }
+    char ch_i = s[left];
+    m[ch_i]++;
+    if(m[ch_i]>0){
+    requiredcount++;
+    }
+    left++;
+      }
+      right++;
+       }
+       return minwindow == INT_MAX?"":s.substr(minstart,minwindow);
     }
 };
