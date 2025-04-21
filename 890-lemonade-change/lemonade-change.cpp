eb992311c6e00d90b54e526888fccb1e8364cd82
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2025-04-21 07:57:14 +0530

    Time: 0 ms (100.00%) | Memory: 87.2 MB (39.94%) - LeetSync

--- 890-lemonade-change/lemonade-change.cpp
+++ 890-lemonade-change/lemonade-change.cpp
@@ -1,32 +1,34 @@
 class Solution {
 public:
     bool lemonadeChange(vector<int>& bills) {
-        int five = 0, ten = 0;
-
-        for (int i = 0; i < bills.size(); i++) {
-            if (bills[i] == 5) {
-                five++;  // Accept $5 bill
-            } 
-            else if (bills[i] == 10) {
-                if (five <= 0) {  // Need one $5 to give change
+        unordered_map<int,int>m;
+        for(auto a:bills){
+        if(a==5){
+        m[a]++;
+        }
+        else if(a==10){
+         if(m[5]<=0){
+          return false;
+         }
+         m[5]--;
+         m[10]++;
+        }
+         else if(a==20){
+         if(m[10]>0 && m[5]>0){
+            m[5]--;
+            m[10]--;
+         }
+         else if(m[5]>=3){
+            m[5]--;
+             m[5]--;
+              m[5]--; 
+         }
+         else {
                     return false;
                 }
-                five--;  // Give one $5 as change
-                ten++;  // Accept $10 bill
-            } 
-            else if (bills[i] == 20) {
-                if (ten > 0 && five > 0) {  // Prioritize giving one $10 and one $5 as change
-                    ten--;
-                    five--;
-                } 
-                else if (five >= 3) {  // If no $10 is available, give three $5 bills as change
-                    five -= 3;
-                } 
-                else {
-                    return false;  // Not enough change available
-                }
-            }
         }
-        return true;  // Successfully provided change for all customers
+        }
+        return true;
+
     }
 };
