7e001f2a2b32781b694da89b064dbc6dc401756d
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2025-04-21 13:21:14 +0530

    Time: 4 ms (19.28%) | Memory: 22.9 MB (44.62%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -1,31 +1,25 @@
-#include <vector>
-#include <algorithm>
-#include <climits>
-using namespace std;
-
 class Solution {
 public:
     int candy(vector<int>& ratings) {
         int n = ratings.size();
-        if (n == 0) return 0;  // Handle edge case of empty ratings
+        if (n == 0) return 0;  
 
-        vector<int> ra(n, 1);  // Start with 1 candy for each child
+        vector<int> ra(n, 1); 
 
-        // Left to right pass
+       
         for (int i = 1; i < n; ++i) {
             if (ratings[i] > ratings[i - 1]) {
                 ra[i] = ra[i - 1] + 1;
             }
         }
 
-        // Right to left pass
+     
         for (int i = n - 2; i >= 0; --i) {
             if (ratings[i] > ratings[i + 1]) {
                 ra[i] = max(ra[i], ra[i + 1] + 1);
             }
         }
 
-        // Calculate the total number of candies
         int ans = 0;
         for (int i = 0; i < n; ++i) {
             ans += ra[i];
