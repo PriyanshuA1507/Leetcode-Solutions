350e16d40a16eb7019c920671afd4cbb7e2fa299
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2025-04-30 06:02:12 +0530

    Time: 100 ms (55.37%) | Memory: 165.5 MB (13.50%) - LeetSync

--- 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.cpp
+++ 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.cpp
@@ -1,46 +1,36 @@
 class Solution {
 public:
     vector<string> findAllRecipes(vector<string>& recipes, vector<vector<string>>& ingredients, vector<string>& supplies) {
-        unordered_set<string> available_supplies(supplies.begin(), supplies.end());
-        unordered_map<string, vector<string>> recipe_to_ingredients;
-        unordered_map<string, int> visited;
-        vector<string> result;
+      unordered_set<string> supplySet(supplies.begin(), supplies.end());
+        unordered_map<string, vector<string>> graph;
+        unordered_map<string, int> indegree;
         
-        for (size_t i = 0; i < recipes.size(); i++) {
-            recipe_to_ingredients[recipes[i]] = ingredients[i];
+        for (int i = 0; i < recipes.size(); ++i) {
+            for (auto& ing : ingredients[i]) {
+                graph[ing].push_back(recipes[i]);
+                indegree[recipes[i]]++;
+            }
         }
-        
-        function<bool(string)> can_make = [&](string recipe) {
-            if (visited.count(recipe)) {
-                return visited[recipe] == 1;
-            }
-            
-            if (available_supplies.count(recipe)) {
-                return true;
-            }
-            
-            if (!recipe_to_ingredients.count(recipe)) {
-                return false;
-            }
-            
-            visited[recipe] = 0;
-            
-            for (const string& ingredient : recipe_to_ingredients[recipe]) {
-                if (!can_make(ingredient)) {
-                    visited[recipe] = -1;
-                    return false;
+
+        queue<string> q;
+        for (auto& item : supplies) {
+            q.push(item);
+        }
+
+        vector<string> ans;
+        while (!q.empty()) {
+            string item = q.front();
+            q.pop();
+
+            for (auto& recipe : graph[item]) {
+                indegree[recipe]--;
+                if (indegree[recipe] == 0) {
+                    ans.push_back(recipe);
+                    q.push(recipe);
                 }
             }
-            
-            visited[recipe] = 1;
-            result.push_back(recipe);
-            return true;
-        };
-        
-        for (const string& recipe : recipes) {
-            can_make(recipe);
         }
-        
-        return result;
+
+        return ans;
     }
 };
