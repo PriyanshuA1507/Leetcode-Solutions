ac73d4ff3db4578bfbb63e79df850ef81abe3295
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2025-06-05 18:27:54 +0530

    Time: 11 ms (56.54%) | Memory: 35.6 MB (73.19%) - LeetSync

--- 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.cpp
+++ 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
     int subarraysDivByK(vector<int>& nums, int k) {
-       int ans = 0;
+        int ans = 0;
         int sum = 0;
-        unordered_map<int, int> prefixSumCount;
-        prefixSumCount[0] = 1;
-
-        for (int num : nums) {
-            sum = (sum + num) % k;
-            if (sum < 0) {
-                sum += k; 
-            }
-            ans += prefixSumCount[sum];
-            prefixSumCount[sum]++;
+        unordered_map<int,int> m;
+        m[0] = 1;
+        for(auto a:nums){
+         sum = (sum+a)%k;
+         if(sum<0){
+          sum+=k;  
+         }
+         
+          ans += m[sum];
+        
+         m[sum]++;
         }
-
         return ans;
     }
 };
-//We need to return the number of subarray that is divisible by a given number k.
-//The logic used here is that when the remainder of 2 subarray array is same then the leftover element of the larger subarray would be divisible by k.
-//For negative remainder if thre is we add k to neuralize. (-2%5 is -2) ((-2+5)%5 is 3)
