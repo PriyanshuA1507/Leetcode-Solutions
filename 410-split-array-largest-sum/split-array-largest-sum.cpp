6c35b5f6117e11fc6b79b8acfbede87f2473785f
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2025-06-11 00:00:04 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (56.62%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/split-array-largest-sum.cpp
@@ -1,42 +1,39 @@
 class Solution {
 public:
-     bool check(int mid,vector<int>& nums, int k){
-     int a = 1;
-     int sum=0;
-     for(int i=0;i<nums.size();i++){
-     if(nums[i]>mid){
-      return false;
-     }
-     if(sum+nums[i]>mid){
-     a++;
-     sum = nums[i];
-     if(a>k){
+    bool check(int mid,vector<int>& nums,int k){
+    int p = 1;
+    int cs = 0;
+    for(auto a:nums){
+      if(cs+a>mid){
+       cs = a;
+       p++;
+       if(p>k){
         return false;
-     }
-     }else{
-    sum+=nums[i];
-     }
-     }
-     return true;
+       }
+      }
+      else{
+        cs+=a;
+      }
+    }
+    return k>=p;
     }
     int splitArray(vector<int>& nums, int k) {
-        int start = *max_element(nums.begin(),nums.end());
-        int end = 0;
-        int ans = 0;
-        for(auto a:nums){
-            end+=a;
-        }
-        while(start<=end){
-        int mid = start + (end-start)/2;
-        if(check(mid,nums,k)){
+    int ans =0;
+     int start = *max_element(nums.begin(),nums.end());
+     int end = 0;
+     for(auto a:nums){
+        end+=a;
+     }
+     while(start<=end){
+    int mid = start + (end-start)/2;
+     if(check(mid,nums,k)){
         ans = mid;
         end = mid-1;
-        }
-        else{
+     }
+     else{
         start = mid+1;
-        }
-        }
-        return ans;
-        
+     }
+     }
+     return ans;   
     }
 };
