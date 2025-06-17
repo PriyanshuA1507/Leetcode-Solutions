1ebb3b319d4bed5fd73d520f924efaeaf9f523f1
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2025-06-17 16:22:05 +0530

    Time: 86 ms (64.18%) | Memory: 238.8 MB (12.48%) - LeetSync

--- 403-frog-jump/frog-jump.cpp
+++ 403-frog-jump/frog-jump.cpp
@@ -1,39 +1,37 @@
 class Solution {
 public:
-    unordered_map<int, int> m;
-    bool solve(vector<int>& stones,int ci,int jumps,vector<vector<int>>& dp){
-    if(ci==stones.size()-1){
-        return true;
+  unordered_map<int, int> stoneToIndex; 
+
+    bool solve(vector<int>& stones, int index, int k, vector<vector<int>>& dp) {
+        if (index == stones.size() - 1) return true;
+
+        if (dp[index][k] != -1) return dp[index][k];
+
+        for (int jump = k - 1; jump <= k + 1; ++jump) {
+            if (jump <= 0) continue;
+
+            int nextPos = stones[index] + jump;
+            if (stoneToIndex.count(nextPos)) {
+                int nextIndex = stoneToIndex[nextPos];
+                if (solve(stones, nextIndex, jump, dp)) {
+                    return dp[index][k] = 1;
+                }
+            }
+        }
+
+        return dp[index][k] = 0;
     }
-    if(dp[ci][jumps]!=-1){
-        return dp[ci][jumps];
-    }
-     bool a = false,b = false,c = false;
-     if( m[stones[ci]+jumps]){
-     a= solve(stones,m[stones[ci]+jumps],jumps,dp);
-    }
-    if(jumps>1 && m[stones[ci]+jumps-1]){
-     b =  solve(stones,m[stones[ci]+jumps-1],jumps-1,dp);
-    }
-    if(m[stones[ci]+jumps+1]){
-     c= solve(stones,m[stones[ci]+jumps+1],jumps+1,dp);
-    }
-    
-    return dp[ci][jumps]=(a||b||c);
-    }
+
     bool canCross(vector<int>& stones) {
         int n = stones.size();
-        int jumps = 0;
-        for(auto a:stones){
-         jumps = max(a,jumps);
+    
+        for (int i = 0; i < n; ++i) {
+            stoneToIndex[stones[i]] = i;
         }
-        vector<vector<int>> dp(n+1,vector<int>(2000+1,-1));
-        if (stones[1] - stones[0] != 1) {
-            return false;
-        }
-         for (int i = 0; i < stones.size(); i++) {
-            m[stones[i]] = i;
-        }
-       return solve(stones,1,1,dp); 
+
+        
+        vector<vector<int>> dp(n, vector<int>(n + 1, -1));
+
+        return solve(stones, 0, 0, dp); 
     }
 };
