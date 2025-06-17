c8013936d48b2cbb675283af0b6ecf26d31f8149
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2025-06-17 15:57:44 +0530

    Time: 199 ms (12.66%) | Memory: 203.9 MB (9.94%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,29 +1,33 @@
 class Solution {
 public:
-   int solve(vector<int>& nums,int sum,int index,vector<vector<int>>& dp){
-    if(sum==0){
-        return 1;
+    bool solve(vector<int>& nums,int index,int target,vector<vector<int>>& dp){
+    if(index==nums.size()-1 && target==0){
+        return true;
     }
-    if(index>=nums.size() || sum<0){
-        return 0;
-    }
-    if(dp[sum][index]!=-1){
-        return dp[sum][index];
-    }
-    bool take = solve(nums,sum-nums[index],index+1,dp);
-    bool nottake = solve(nums,sum,index+1,dp);
-    return dp[sum][index]=(take||nottake);
-   }
-    bool canPartition(vector<int>& nums) {
-      int n = nums.size();
-      int sum = 0;
-      for( int a :nums){
-        sum+=a;
-      }   
-      if(sum%2!=0){
+    if(target<0 || index>=nums.size()){
         return false;
-      }
-      vector<vector<int>> dp(sum/2+1,vector<int>(n+1,-1));
-      return solve(nums,sum/2,0,dp);
+    }
+    if(dp[index][target]!=-1){
+        return dp[index][target];
+    }
+    bool take;
+    if(index+1<nums.size()){
+   take = solve(nums,index+1,target-nums[index],dp);
+    }
+    bool nottake = solve(nums,index+1,target,dp);
+
+    return dp[index][target] = take||nottake;
+    }
+    bool canPartition(vector<int>& nums) {
+        int n = nums.size();
+     int sum = 0;
+     for(auto a:nums){
+     sum+=a;
+     }   
+     if(sum%2!=0){
+        return false;
+     }
+     vector<vector<int>> dp(n,vector<int>(sum,-1));
+     return solve(nums,0,sum/2,dp);
     }
 };
