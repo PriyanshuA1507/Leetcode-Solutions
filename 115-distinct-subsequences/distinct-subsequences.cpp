37bff6f893e010c9cf374847c481e3960dfe898e
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2025-06-19 11:33:50 +0530

    Time: 43 ms (14.60%) | Memory: 27.1 MB (46.20%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,25 +1,34 @@
 class Solution {
 public:
-    int solve(string s, string t,int i,int j, vector<vector<int>>& dp){
-        if(j>=t.size()){
-            return 1;
-        }
-        if(i>=s.size()){
-            return 0;
-        }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        int b = 0;
-        if(s[i]==t[j]){
-            b= solve(s,t,i+1,j+1,dp);
-        }
-        int a= solve(s,t,i+1,j,dp);
-        return dp[i][j]=a+b;
+    int solve(string& s,string& t,int index1,int index2, vector<vector<int>>& dp){
+    if(index1>=s.size()){
+     if(index2>=t.size()){
+        return 1;
+     }
+     else{
+        return 0;
+     }
+    }
+    if(index2>=t.size()){
+        return 1;
+    }
+    if(dp[index1][index2]!=-1){
+        return dp[index1][index2];
+    }
+    int take = 0,nottake = 0;
+
+    if(s[index1]==t[index2]){
+    take = solve(s,t,index1+1,index2+1,dp);
+    nottake = solve(s,t,index1+1,index2,dp);
+    }
+    else{
+    return solve(s,t,index1+1,index2,dp);
+    }
+
+    return dp[index1][index2]= take+nottake;
     }
     int numDistinct(string s, string t) {
-        int n = max(s.size(),t.size());
-        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
-         return solve(s,t,0,0,dp);
+        vector<vector<int>> dp(s.size()+1,vector<int>(t.size()+1,-1));
+        return solve(s,t,0,0,dp);
     }
 };
