44c6232f314dd0f03e995d244a60c62c2d12a8e2
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2025-06-30 18:00:03 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (31.45%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
-    int findPeakElement(vector<int>& arr) {
-      int n = arr.size();
-      int start = 0;
-      int end = n-1;
-      while(start<end){
-     int mid = start + (end-start)/2;
-     if(arr[mid]<arr[mid+1]){
-     start = mid+1;
-     }
-     else{
-        end = mid;
-     }
-      }
-      return start;
+    int findPeakElement(vector<int>& nums) {
+       int start = 0;
+       int end = nums.size()-1;
+       while(start<end){
+       int mid = start + (end-start)/2;
+       if(nums[mid]<nums[mid+1]){
+        start = mid+1;
+       }
+       else{
+       end = mid;
+       }
+       }
+       return start; 
     }
 };
