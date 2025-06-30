ac7ba4da950ea60b6b25bbb1e03c214da73ffd67
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2025-06-30 10:42:25 +0530

    Time: 18 ms (12.83%) | Memory: 40.9 MB (5.14%) - LeetSync

--- 113-path-sum-ii/path-sum-ii.cpp
+++ 113-path-sum-ii/path-sum-ii.cpp
@@ -11,25 +11,27 @@
  */
 class Solution {
 public:
- vector<vector<int>> ans;
- void dfs(TreeNode* root, int targetSum, vector<int>& currentPath) {
-        if (!root) return;
+   void solve(TreeNode* root,int ts,vector<int> a,vector<vector<int>>& ans){
+   if(!root){
+    return;
+   }
 
-        currentPath.push_back(root->val);
+   a.push_back(root->val);
 
-        if (!root->left && !root->right && root->val == targetSum) {
-            ans.push_back(currentPath);
-        }
-
-        dfs(root->left, targetSum - root->val, currentPath);
-        dfs(root->right, targetSum - root->val, currentPath);
-
-        currentPath.pop_back();  
+    if(!root->left && !root->right && ts==root->val){
+    ans.push_back(a);
     }
+
+    solve(root->left, ts-root->val,a,ans);
+    solve(root->right,ts-root->val,a,ans);
+
+  a.pop_back();
+
+   }
     vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
-     
-        vector<int> currentPath;
-        dfs(root, targetSum, currentPath);
-        return ans;
+    vector<int> a;
+    vector<vector<int>> ans;
+    solve(root,targetSum,a,ans);
+    return ans;
     }
 };
