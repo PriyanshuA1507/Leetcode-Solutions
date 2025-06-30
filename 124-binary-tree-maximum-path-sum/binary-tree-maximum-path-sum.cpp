657bfe4c9537c305f176cc9218d4e5ddccda5167
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2025-06-30 13:00:47 +0530

    Time: 4 ms (8.99%) | Memory: 28 MB (24.66%) - LeetSync

--- 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.cpp
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.cpp
@@ -11,24 +11,24 @@
  */
 class Solution {
 public:
-    int maxsum;
-    int solve(TreeNode* root){
-    if(!root){
-        return 0;
-    }
-    int l = solve(root->left);
-    int r = solve(root->right);
+int maxsum;
+   int solve(TreeNode* root){
+   if(!root){
+    return 0;
+   }
+  int l = solve(root->left);
+  int r = solve(root->right);
 
-    int neeche_hi_milgya_answer = l+ r + root->val;
-     int koi_ek_acha = max(l,r) + root->val;
-     int only_root_acha = root->val;
-     maxsum = max({maxsum,neeche_hi_milgya_answer,koi_ek_acha,only_root_acha});
+ int neechehiacha = l + r + root->val;
+ int koiekacha = max(l,r) + root->val;
+ int onlyrootacha = root->val;
+ maxsum = max({maxsum,neechehiacha,koiekacha,onlyrootacha});
 
-     return max({koi_ek_acha,only_root_acha});
-    }
+  return max({koiekacha,onlyrootacha});
+   }
     int maxPathSum(TreeNode* root) {
-       maxsum = INT_MIN;
-      solve(root);
-       return maxsum; 
+    maxsum = INT_MIN;
+    solve(root);
+    return maxsum;
     }
 };
