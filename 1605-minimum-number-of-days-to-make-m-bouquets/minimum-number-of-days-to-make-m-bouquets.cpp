a15dff4931094cb276085e266b380411147ff256
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2025-06-30 18:29:30 +0530

    Time: 21 ms (66.01%) | Memory: 70.5 MB (46.57%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,40 +1,42 @@
 class Solution {
 public:
-    bool check(int mid,vector<int>& bloomDay, int m, int k){
-    int bouquets = 0;
-        int flowers = 0;
-        
-        for (int i = 0; i < bloomDay.size(); i++) {
-            if (bloomDay[i] <= mid) {
-                flowers++;
-                if (flowers == k) { 
-                    bouquets++;
-                    flowers = 0; 
-                }
-            } else {
-                flowers = 0; 
-            }
+     bool cp(vector<int>& b, int m, int k,int mid){
+    int bo = 0;
+    int f = 0;
+    for(auto c:b){
+     if(c<=mid){
+        f++;
+        if(f==k){
+        bo++;
+        f=0;
         }
-        
-        return bouquets >= m;  
+     }else{
+        f = 0;
+     }
     }
-    int minDays(vector<int>& bloomDay, int m, int k) {
-        if((long long)m*k>bloomDay.size()){
-            return -1;
-        }
-        int start = 1;
-        int end = *max_element(bloomDay.begin(), bloomDay.end());
-        int ans = -1;
-        while(start<=end){
-            int mid = (start) + (end-start)/2;
-            if(check(mid,bloomDay,m,k)){
-                ans = mid;
-                end = mid-1;
-            }
-            else{
-                start = mid+1;
-            }
-        }
-        return ans;
+    return bo>=m;
+     }
+    int minDays(vector<int>& b, int m, int k) {
+      if(b.size()<(long long)m*k){
+        return -1;
+      }  
+      int ans = 0;
+      int start = 1;
+      int end = 0;
+      for(auto a:b){
+        end = max(end,a);
+      }
+    
+      while(start<=end){
+     int mid = start + (end-start)/2;
+     if(cp(b,m,k,mid)){
+     ans = mid;
+     end = mid-1;
+     }
+     else{
+        start = mid+1;
+     }
+      }
+      return ans;
     }
 };
