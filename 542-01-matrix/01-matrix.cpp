36d9993118cab8b337232cf2682a7dc4baa86142
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2025-07-09 17:42:46 +0530

    Time: 183 ms (5.01%) | Memory: 76.4 MB (5.03%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -1,34 +1,40 @@
 class Solution {
 public:
-   using p = pair<pair<int,int>,int>;
-   vector<vector<int>> dir = {{-1,0},{0,1},{1,0},{0,-1}};
+ using p = pair<pair<int,int>,int>;
+vector<vector<int>> dir = {{-1,0},{0,1},{1,0},{0,-1}};
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-     queue<p> q;
-     vector<vector<int>> time(mat.size(),vector<int>(mat[0].size(),-1));
-     for(int i=0;i<mat.size();i++){
-    for(int j=0;j<mat[0].size();j++){
-    if(mat[i][j]==0){
-    q.push({{i,j},0});
-    time[i][j] = 0;
-    }
-    }
-     }
-     while(q.size()!=0){
-        int a = q.size();
-    for(int i=0;i<a;i++){
-    auto b = q.front();
-    q.pop();
-    for(auto d:dir){
-    int i = b.first.first + d[0];
-    int j = b.first.second + d[1];
+        queue<p> q;
+      int n = mat.size(), m = mat[0].size();
+       vector<vector<int>> dist(n,vector<int>(m,1e8));
+       unordered_map<int,vector<int>> adj;
+       for(int i=0;i<n;i++){
+      for(int j=0;j<m;j++){
+      if(mat[i][j]==0){
+    dist[i][j] = 0;
+     q.push({{i,j},0});
+      }
+      }
+       }
 
-    if(i>=0 && j>=0 && i<mat.size() && j<mat[0].size() && time[i][j]==-1){
-     time[i][j] = b.second + 1;
-    q.push({{i, j}, b.second + 1});
-    }
-    }
-    }
-     }
-     return time;   
+       while(q.size()!=0){
+        auto s = q.size();
+        for(int i=0;i<s;i++){
+        auto a = q.front();
+        q.pop();
+        int row = a.first.first, col = a.first.second;
+        int dista = a.second;
+        for(auto d:dir){
+        int nr = row + d[0];
+        int nc = col + d[1];
+
+        if(nr>=0 && nc>=0 && nr<n && nc<m && mat[nr][nc]==1){
+        dist[nr][nc] = 1 + dista;
+        mat[nr][nc] = 0;
+        q.push({{nr,nc},dist[nr][nc]});
+        }
+        }
+        }
+       }
+       return dist;
     }
 };
