df1c697ab6ff68dd16839dc710421c91bf790fcc
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2025-07-10 00:32:05 +0530

    Time: 7 ms (49.63%) | Memory: 20.9 MB (12.67%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,37 +1,38 @@
 class Solution {
 public:
-    bool canFinish(int nc, vector<vector<int>>& prerequisites) {
-      vector<int> indegree(nc,0);
-        unordered_map<int,vector<int>>adj;
+    bool canFinish(int n, vector<vector<int>>& p) {
         queue<int> q;
-        for(auto a:prerequisites){
-          int u = a[0];
-          int v = a[1];
-          adj[u].push_back(v);
-          indegree[v]++;
-                }
-        for(int i=0;i<nc;i++){
-            if(indegree[i]==0){
-                q.push(i);
-            }
-        }
-        int count = 0;
-        while(q.size()!=0){
-         int a = q.front();
-         q.pop();
-         count++;
-         for(auto b:adj[a]){
-          indegree[b]--;
-          if(indegree[b]==0){
-           q.push(b);
-          }
-         }
-        }
-        if(count==nc){
-            return true;
-        }
+    int count = 0;
+    vector<int> indegree(n,0);
+     unordered_map<int,vector<int>> adj;
+     for(auto a:p){
+     int u = a[0], v = a[1];
+     adj[u].push_back(v);
+     indegree[v]++;
+     }   
+     
+     for(int i=0;i<n;i++){
+     if(indegree[i]==0){
+      count++;
+      q.push(i);
+     }
+     }
 
-         return false;
-           
+     while(q.size()!=0){
+      auto a = q.front();
+      q.pop();
+      for(auto b:adj[a]){
+      indegree[b]--;
+      if(indegree[b]==0){
+      q.push(b);
+      count++;
+      }
+      }
+     }
+     if(count==n){
+        return true;
+     }
+     return false;
+
     }
 };
