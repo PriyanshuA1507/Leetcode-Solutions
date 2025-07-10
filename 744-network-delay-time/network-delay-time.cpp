618f8a44bacad4314a3a92fc8f9e91837535bbf7
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2025-07-10 11:29:44 +0530

    Time: 98 ms (20.17%) | Memory: 47.9 MB (36.53%) - LeetSync

--- 744-network-delay-time/network-delay-time.cpp
+++ 744-network-delay-time/network-delay-time.cpp
@@ -1,47 +1,35 @@
 class Solution {
 public:
+using p = pair<int,int>;
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        unordered_map<int, vector<pair<int, int>>> adj;
-
-        // Building the adjacency list for the directed graph
-        for (int i = 0; i < times.size(); i++) {
-            int u = times[i][0];
-            int v = times[i][1];
-            int w = times[i][2];
-            adj[u].push_back({v, w});
+        vector<int> dist(n+1,1e8);
+        unordered_map<int,vector<p>> adj;
+        for(auto a:times){
+        int u = a[0], v = a[1], w = a[2];
+        adj[u].push_back({v,w});
+        }
+        priority_queue<p,vector<p>,greater<p>> q;
+        dist[k] = 0;
+        q.push({0,k});
+        while(q.size()!=0){
+        auto a = q.top();
+        int d = a.first;
+        q.pop();
+        if(d>dist[a.second])continue;
+        for(auto b:adj[a.second]){
+        if(d+b.second<dist[b.first]){
+        dist[b.first] = d+b.second;
+        q.push({dist[b.first],b.first});
+        }
+        }
         }
 
-        // Min-heap to store {distance, node}
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-        
-        // Initializing result vector with size n+1 to accommodate nodes from 1 to n
-        vector<int> result(n + 1, INT_MAX);
-        result[k] = 0;  // Start from node k
-        pq.push({0, k});
-
-        // Dijkstra's algorithm
-        while (!pq.empty()) {
-            auto a = pq.top();
-            pq.pop();
-            int dist1 = a.first;
-            int node = a.second;
-
-            for (auto& b : adj[node]) {
-                int nnode = b.first;
-                int dist2 = b.second;
-                if (dist1 + dist2 < result[nnode]) {
-                    result[nnode] = dist1 + dist2;
-                    pq.push({result[nnode], nnode});
-                }
-            }
-        }
-         int ans = INT_MIN;
+        int ans = -1;
         for (int i = 1; i <= n; i++) {
-            if (result[i] == INT_MAX) {
-                return -1;
-            }
-            ans = max(ans, result[i]);
+            if (dist[i] == 1e8) return -1; 
+            ans = max(ans, dist[i]);
         }
+
         return ans;
     }
 };
