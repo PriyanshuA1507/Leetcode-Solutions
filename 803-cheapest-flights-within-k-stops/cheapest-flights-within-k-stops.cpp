f00e0d5d1d52f9df974f6286616c8d9b78a5b4cf
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2025-07-10 12:03:06 +0530

    Time: 4 ms (46.46%) | Memory: 19 MB (22.67%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -1,51 +1,37 @@
 class Solution {
 public:
-  using p = pair<int,pair<int,int>>;
+    using p = pair<int,pair<int,int>>;
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-      unordered_map<int, vector<pair<int, int>>> adj;
-        
-        for (int i = 0; i < flights.size(); i++) {
-            int u = flights[i][0];
-            int v = flights[i][1];
-            int w = flights[i][2];
-            adj[u].push_back({v, w});
-        }
-        
-        priority_queue<p, vector<p>, greater<p>> q;
-        q.push({0, {src, 0}});
-        
-        // Track the minimum cost to reach each node with at most `k` stops
-        vector<vector<int>> result(n, vector<int>(k + 2, INT_MAX));
-        result[src][0] = 0;
-        
-        while (!q.empty()) {
-            auto current = q.top();
-            q.pop();
-            
-            int currCost = current.first;
-            int node = current.second.first;
-            int stops = current.second.second;
-            
-            // If we reached the destination, return the cost
-            if (node == dst) return currCost;
-            
-            // If we have used up all stops, continue
-            if (stops > k) continue;
-            
-            // Explore all the neighbors of the current node
-            for (auto& neighbor : adj[node]) {
-                int nextNode = neighbor.first;
-                int nextCost = neighbor.second;
-                
-                // If a cheaper path is found, push it to the queue
-                if (currCost + nextCost < result[nextNode][stops + 1]) {
-                    result[nextNode][stops + 1] = currCost + nextCost;
-                    q.push({currCost + nextCost, {nextNode, stops + 1}});
-                }
-            }
-        }
-        
-        // If we can't reach the destination
-        return -1;
+    unordered_map<int,vector<pair<int,int>>> adj;
+    vector<vector<int>> ans(n,vector<int>(k+2,1e8));
+    for(auto a:flights){
+    int u = a[0];
+    int v = a[1];
+    int w = a[2];
+    adj[u].push_back({v,w});
+    }
+    priority_queue<p,vector<p>,greater<p>> q;
+    q.push({0,{src,0}});
+    ans[src][0] = 0;
+    while(q.size()!=0){
+    auto a = q.top();
+    q.pop();
+    int distp = a.first;
+    int k1 = a.second.second;
+    int node = a.second.first;
+    if(dst==node){
+        return distp;
+    }
+    if(k1>k){
+    continue;
+    }
+    for(auto b:adj[node]){
+    if(distp+b.second<ans[b.first][k1+1]){
+   ans[b.first][k1+1] = distp + b.second;
+    q.push({ans[b.first][k1+1],{b.first,k1+1}});
+    }
+    }
+    }
+    return -1;
     }
 };
