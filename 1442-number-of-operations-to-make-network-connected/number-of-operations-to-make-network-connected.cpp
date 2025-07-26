746a9d18a895e398199118891f2b75ae06496725
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2025-07-26 21:34:54 +0530

    Time: 19 ms (56.48%) | Memory: 47.1 MB (37.98%) - LeetSync

--- 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
@@ -1,49 +1,76 @@
 class Solution {
 public:
-   void dfs(int u, vector<vector<int>>& adj, vector<bool>& visited) {
-       stack<int> st;
-       st.push(u);
-       
-       while (!st.empty()) {
-           int node = st.top();
-           st.pop();
-           
-           if (visited[node]) continue;
-           visited[node] = true;
+class DisjointSet {
+private:
+    vector<int> rank, parent, size;
 
-           for (int v : adj[node]) {
-               if (!visited[v]) {
-                   st.push(v);
-               }
-           }
-       }
-   }
+public:
+    DisjointSet(int n) {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        size.resize(n + 1, 1); 
 
-   int makeConnected(int n, vector<vector<int>>& connections) {
-       if (connections.size() < n - 1) {
-           return -1;  
-       }
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+        }
+    }
 
-     
-       vector<vector<int>> adj(n);
-       
-       for (const auto& conn : connections) {
-           int u = conn[0];
-           int v = conn[1];
-           adj[u].push_back(v);
-           adj[v].push_back(u);
-       }
+    int findparent(int node) {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findparent(parent[node]); 
+    }
 
-       vector<bool> visited(n, false);
-       int count = 0;
+    void unionbyrank(int u, int v) {
+        int pu = findparent(u);
+        int pv = findparent(v);
 
-       for (int i = 0; i < n; ++i) {
-           if (!visited[i]) {
-               count++;
-               dfs(i, adj, visited);
-           }
-       }
+        if (pu == pv) return;
 
-       return count - 1;  
-   }
+        if (rank[pu] > rank[pv]) {
+            parent[pv] = pu;
+        } else if (rank[pu] < rank[pv]) {
+            parent[pu] = pv;
+        } else {
+            parent[pv] = pu;
+            rank[pu]++;
+        }
+    }
+
+    void unionbysize(int u, int v) {
+        int pu = findparent(u);
+        int pv = findparent(v);
+
+        if (pu == pv) return;
+
+        if (size[pu] > size[pv]) {
+            parent[pv] = pu;
+            size[pu] += size[pv];
+        } else {
+            parent[pu] = pv;
+            size[pv] += size[pu];
+        }
+    }
+     int countComponents(int n) {
+            int count = 0;
+            for (int i = 0; i < n; i++) {
+                if (parent[i] == i) count++;
+            }
+            return count;
+        }
+};
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        if(connections.size()<n-1){
+            return -1;
+        }
+
+        DisjointSet dsu(n);
+        int ans = 0;
+        for(auto a:connections){
+         int u = a[0], v = a[1];
+         dsu.unionbysize(u,v);
+        }
+        ans = dsu.countComponents(n);
+        return ans-1;
+    }
 };
