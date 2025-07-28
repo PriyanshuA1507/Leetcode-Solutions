b21cec5a35e8635d6d9bee113254348bbfa54ee3
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2025-07-28 10:11:49 +0530

    Time: 9 ms (20.79%) | Memory: 15.3 MB (82.62%) - LeetSync

--- 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.cpp
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.cpp
@@ -1,52 +1,70 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
 class Solution {
 public:
-    void buildgraph(TreeNode* root, unordered_map<int, vector<int>>& adj) { 
-        if (!root) {
-            return;
-        }
-        if (root->left) {
-            adj[root->val].emplace_back(root->left->val);
-            adj[root->left->val].emplace_back(root->val);
-            buildgraph(root->left, adj);
-        }
-        if (root->right) {
-            adj[root->val].emplace_back(root->right->val);
-            adj[root->right->val].emplace_back(root->val);
-            buildgraph(root->right, adj);
-        }
-    }
-    
+   unordered_map<TreeNode*, TreeNode*> parent;
+   void addparent(TreeNode* root){
+   if(!root){
+    return;
+   }
+
+   if(root->left)
+   parent[root->left] = root;
+   addparent(root->left);
+
+   if(root->right)
+   parent[root->right] = root;
+   addparent(root->right);
+   }
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
-        unordered_map<int, vector<int>> adj;
-        buildgraph(root, adj); 
-        queue<int> q;
-        unordered_map<int, bool> visited;
-        visited[target->val] = true;
-        q.push(target->val);
-        int level = 0;
-        vector<int> ans;
-        
-        while (!q.empty()) {
-            int a = q.size();
-            if (level == k) {
-                while (!q.empty()) {
-                    ans.push_back(q.front());
-                    q.pop();
-                }
-                break; 
-            }
-            for (int i = 0; i < a; i++) {
-                int b = q.front();
-                q.pop();
-                for (int n : adj[b]) {
-                    if (!visited[n]) {
-                        q.push(n);
-                        visited[n] = true;
-                    }
-                }
-            }
-            level++;
-        }
-        return ans;
+    addparent(root);
+    vector<int> result;
+    queue<TreeNode*> q;
+    
+    unordered_set<int> visited;
+    
+         q.push(target);
+        visited.insert(target->val);
+
+    while(q.size()!=0){
+    int n = q.size();
+    if(k==0){
+        break;
+    }
+    while(n--){
+    TreeNode* curr = q.front();
+    q.pop();
+
+    if(curr->left && !visited.count(curr->left->val)){
+    q.push(curr->left);
+    visited.insert(curr->left->val);
+    }
+
+    if(curr->right && !visited.count(curr->right->val)){
+    q.push(curr->right);
+    visited.insert(curr->right->val);
+    }
+
+    if(parent.count(curr) && !visited.count(parent[curr]->val)){
+    q.push(parent[curr]);
+    visited.insert(parent[curr]->val);
+    }
+    }
+    k--;
+    }
+
+    while(q.size()!=0){
+    TreeNode* temp = q.front();
+    q.pop();
+    result.push_back(temp->val);
+    }
+    return result;
     }
 };
