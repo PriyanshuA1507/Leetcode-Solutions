46fdd9838bbb797fe93dd098e364ef47c92f61e0
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2025-07-28 22:51:46 +0530

    Time: 0 ms (100.00%) | Memory: 22.2 MB (42.30%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.cpp
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -1,40 +1,34 @@
 class Solution {
 public:
-    vector<int> asteroidCollision(vector<int>& a) {
-        stack<int> st;
-        for(int i = 0; i < a.size(); i++) {
-            bool destroyed = false;
-            
-            // Process collisions while there are asteroids in the stack and the current asteroid is negative
-            while (!st.empty() && a[i] < 0 && st.top() > 0) {
-                if (st.top() < -a[i]) {  
-                    // Top asteroid is smaller, so destroy it
-                    st.pop();
-                    continue;  // Check again with the next top element
-                } 
-                else if (st.top() == -a[i]) {  
-                    // Both asteroids are equal, destroy both
-                    st.pop();
-                }
-                
-                // If top asteroid is larger, the current asteroid is destroyed
-                destroyed = true;
-                break;
-            }
-            
-            // If not destroyed, push to stack
-            if (!destroyed) {
-                st.push(a[i]);
-            }
-        }
-        
-        // Convert stack to result vector
+    vector<int> asteroidCollision(vector<int>& arr) {
+        int n = arr.size();
         vector<int> ans;
-        while (!st.empty()) {
-            ans.push_back(st.top());
+        stack<int> st;
+        for(int i=0;i<n;i++){
+        bool burst = false;
+        while(st.size()!=0 && st.top()>0 && arr[i]<0){
+         if(st.top() == -arr[i]){
             st.pop();
+         }
+         else if(st.top() < -arr[i]){
+          st.pop();
+          continue;
+         }
+         burst = true;
+         break;
         }
-        reverse(ans.begin(), ans.end());
+        if(!burst){
+        st.push(arr[i]);
+        }
+        }
+      
+       while(st.size()!=0){
+        ans.push_back(st.top());
+        st.pop();
+        }
+        reverse(ans.begin(),ans.end());
         return ans;
+
+
     }
 };
