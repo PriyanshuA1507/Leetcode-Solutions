7851bbf32b2212c494782238d6ff82b9a6a6723b
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2025-08-01 02:01:22 +0530

    Time: 35 ms (69.56%) | Memory: 32.7 MB (19.28%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.cpp
+++ 876-hand-of-straights/hand-of-straights.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
+
     bool isNStraightHand(vector<int>& hand, int gs) {
-    priority_queue<int,vector<int>,greater<int>>pq;
-     if(hand.size()%gs!=0){
-        return false;
-     }
-     unordered_map<int,int>m;
-     for(auto a:hand){
-        pq.push(a);
-        m[a]++;
-     }
-     while(pq.size()!=0){
-       int start = pq.top();
+        priority_queue<int,vector<int>,greater<int>> pq;
+        unordered_map<int,int> m;
+        for(int i=0;i<hand.size();i++){
+          pq.push(hand[i]);
+         m[hand[i]]++;
+        }
+        while(pq.size()!=0){
+        int start = pq.top();
         pq.pop();
-      if(m[start]==0){
-       continue;
-      }
-      for(int i=0;i<gs;i++){
-      if(m[start+i]<=0){
+        if(m[start]==0){
+        continue;
+        }
+        for(int i=0;i<gs;i++){
+        if(m[start]==0){
         return false;
-      }
-      m[start+i]--;
-      }
-     }
-     return true;
+        }
+        m[start]--;
+        start++;
+        }
+        }
+        return true;
     }
 };
