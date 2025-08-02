0353cf740f9730b8c0589202b16609ece8782f0d
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2025-08-02 16:28:39 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (92.89%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -1,30 +1,28 @@
 class Solution {
 public:
     bool checkValidString(string s) {
-        int minOpen = 0; // minimum possible open parentheses
-        int maxOpen = 0; // maximum possible open parentheses
-
-        for (char ch : s) {
-            if (ch == '(') {
-                minOpen++;
-                maxOpen++;
-            } else if (ch == ')') {
-                minOpen--;
-                maxOpen--;
-            } else { // ch == '*'
-                minOpen--; // '*' can be treated as ')'
-                maxOpen++; // '*' can be treated as '('
-            }
-
-            if (maxOpen < 0) {
-                return false; // More closing brackets than opening
-            }
-
-            if (minOpen < 0) {
-                minOpen = 0; // Reset minOpen to 0 because it can't be negative
-            }
+        int minopen = 0;
+        int maxopen = 0;
+        for(auto ch:s){
+        if(ch == '('){
+         minopen++;
+         maxopen++;
         }
-
-        return minOpen == 0; // Ensure all open parentheses are closed
+        else if(ch == ')'){
+        minopen--;
+        maxopen--;
+        }
+        else{
+        minopen--;
+        maxopen++;
+        }
+        if(maxopen<0){
+            return false;
+        }
+        if(minopen<0){
+        minopen=0;
+        }
+        }
+        return minopen==0;
     }
 };
