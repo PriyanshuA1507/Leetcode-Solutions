71da1448935c0dd562bfe98ebbe57831596b0c3a
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2025-08-05 11:55:58 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (23.06%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/split-array-largest-sum.cpp
@@ -1,39 +1,39 @@
 class Solution {
 public:
-    bool check(int mid,vector<int>& nums,int k){
+    bool possible(vector<int>& nums,int k,int mid){
     int p = 1;
-    int cs = 0;
+    int sum = 0;
     for(auto a:nums){
-      if(cs+a>mid){
-       cs = a;
-       p++;
-       if(p>k){
+     if(a>mid){
         return false;
-       }
-      }
-      else{
-        cs+=a;
-      }
-    }
-    return k>=p;
-    }
-    int splitArray(vector<int>& nums, int k) {
-    int ans =0;
-     int start = *max_element(nums.begin(),nums.end());
-     int end = 0;
-     for(auto a:nums){
-        end+=a;
      }
-     while(start<=end){
-    int mid = start + (end-start)/2;
-     if(check(mid,nums,k)){
-        ans = mid;
-        end = mid-1;
+     if(sum + a > mid){
+      p++;
+      sum = a;
      }
      else{
-        start = mid+1;
+        sum+=a;
+     }
+    }
+    return p <= k;
+    }
+    int splitArray(vector<int>& nums, int k) {
+     auto mini = *std::max_element(nums.begin(), nums.end());
+     int maxi = 0;
+     for(auto a:nums){
+      maxi += a;
+     }
+     int ans = maxi;
+     while(mini<=maxi){
+     int mid = mini + (maxi-mini)/2;
+     if(possible(nums,k,mid)){
+     ans = min(ans,mid);
+     maxi = mid-1;
+     }
+     else{
+        mini = mid + 1;
      }
      }
-     return ans;   
+        return ans;
     }
 };
